84707afeaf0fbcd97dff42c9de4012adbc706b8f
Muga Nishizawa <muga.nishizawa@gmail.com>
2014-10-03 15:36:00 -0700

    LocalFileCsvOutput: implemented with RecordCursor and RecordConsumer

--- quickload-core/src/main/java/org/quickload/out/LocalFileCsvOutput.java
+++ quickload-core/src/main/java/org/quickload/out/LocalFileCsvOutput.java
@@ -3,20 +3,27 @@ package org.quickload.out;
 import org.quickload.config.Config;
 import org.quickload.config.ConfigSource;
 import org.quickload.config.DynamicModel;
-import org.quickload.record.Page;
+import org.quickload.exec.BufferManager;
+import org.quickload.record.*;
 import org.quickload.spi.*;
 
+import java.io.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class LocalFileCsvOutput
         extends BasicOutputPlugin<LocalFileCsvOutput.Task>
-{
+{ // TODO change superclass to FileOutputPlugin
+
     // TODO use DynamicTask
     public interface Task
             extends OutputTask, DynamicModel<Task>
-    {
-        @Config("paths")
+    { // TODO change superclass to FileOutputTask
+        @Config("out:paths")
         public List<String> getPaths();
+
+        @Config("out:schema")
+        public Schema getSchema();
     }
 
     public static class Operator
@@ -24,17 +31,66 @@ public class LocalFileCsvOutput
     {
         private final Task task;
         private final int processorIndex;
+        private final PageAllocator pageAllocator;
 
         Operator(Task task, int processorIndex) {
             this.task = task;
             this.processorIndex = processorIndex;
+            this.pageAllocator = new BufferManager(); // TODO
         }
 
         @Override
         public void addPage(Page page)
         {
-            int len = page.length();
-            // ...
+            // TODO ad-hoc
+            String path = task.getPaths().get(processorIndex);
+            // TODO manually create schema object now
+            Schema schema = new Schema(Arrays.asList(
+                    new Column(0, "date_code", StringType.STRING),
+                    new Column(1, "customer_code", StringType.STRING),
+                    new Column(2, "product_code", StringType.STRING),
+                    new Column(3, "employee_code", StringType.STRING)));
+
+            // TODO simple implementation
+
+            PageReader pageReader = new PageReader(pageAllocator, schema);
+            RecordCursor recordCursor = pageReader.cursor(page);
+            File file = new File(path);
+
+            try (PrintWriter w = new PrintWriter(file)) {
+                // TODO writing data to the file
+
+                while (recordCursor.next()) {
+                    RecordConsumer recordConsumer = new RecordConsumer() {
+                        @Override
+                        public void setNull(Column column) {
+                            // TODO
+                        }
+
+                        @Override
+                        public void setLong(Column column, long value) {
+                            // TODO
+                        }
+
+                        @Override
+                        public void setDouble(Column column, double value) {
+                            // TODO
+                        }
+
+                        @Override
+                        public void setString(Column column, String value) {
+                            System.out.print(value);
+                            System.out.print(',');
+                            //w.append(value).append(',');
+                        }
+                    };
+                    schema.consume(recordCursor, recordConsumer);
+                    System.out.println();
+                    //w.append('\n');
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
