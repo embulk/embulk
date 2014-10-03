4de0b669e04c20fe532b94837fce4535e8dbab40
Muga Nishizawa <muga.nishizawa@gmail.com>
2014-10-03 15:35:18 -0700

    LocalFileCsvInput: implemented it with RecordBuilder and RecordProducer

--- quickload-core/src/main/java/org/quickload/in/LocalFileCsvInput.java
+++ quickload-core/src/main/java/org/quickload/in/LocalFileCsvInput.java
@@ -4,33 +4,49 @@ import org.quickload.config.Config;
 import org.quickload.config.ConfigSource;
 import org.quickload.config.DynamicModel;
 import org.quickload.exec.BufferManager;
-import org.quickload.record.Page;
+import org.quickload.record.Column;
+import org.quickload.record.DoubleType;
+import org.quickload.record.LongType;
+import org.quickload.record.PageAllocator;
 import org.quickload.record.PageBuilder;
 import org.quickload.record.RecordBuilder;
+import org.quickload.record.RecordProducer;
 import org.quickload.record.Schema;
-import org.quickload.spi.*;
+import org.quickload.record.StringType;
+import org.quickload.spi.BasicInputPlugin;
+import org.quickload.spi.DynamicReport;
+import org.quickload.spi.InputProgress;
+import org.quickload.spi.InputTask;
+import org.quickload.spi.OutputOperator;
+import org.quickload.spi.ReportBuilder;
+import org.quickload.spi.ThreadInputProcessor;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
 
 public class LocalFileCsvInput
         extends BasicInputPlugin<LocalFileCsvInput.Task>
-{
+{ // TODO change superclass to FileInputPlugin
+
     public interface Task
             extends InputTask, DynamicModel<Task>
-    {
-        @Config("paths")
+    { // TODO change superclass to FileInputTask
+        @Config("in:paths") // TODO temporarily added 'in:'
         public List<String> getPaths();
+
+        @Config("in:schema") // TODO temporarily added 'in:'
+        public Schema getSchema();
     }
 
     public static class Processor
             extends ThreadInputProcessor
     {
-        private final Task task;
+        private final Task task; // TODO change FileInputTask
         private final int processorIndex;
-        private final BufferManager bufferManager;
+        private final PageAllocator pageAllocator;
 
         public Processor(Task task,
                 int processorIndex, OutputOperator op)
@@ -38,47 +54,60 @@ public class LocalFileCsvInput
             super(op);
             this.task = task;
             this.processorIndex = processorIndex;
-            this.thread.start(); // TODO when can we start the thread??
-            this.bufferManager = new BufferManager();
+             // TODO Sada push the refactoring related to ThreadInProcessor
+            this.thread.start();
+            // TODO initialize page allocator object
+            // TODO consider when the page allocator object is released?
+            this.pageAllocator = new BufferManager(); // TODO
         }
 
         @Override
         public ReportBuilder runThread() throws Exception
         {
-            String path = task.getPaths().get(processorIndex); // TODO ad hoc
-            System.out.println("## path: " + path);
-            Schema schema = task.getSchema(); // TODO doesn't work
-            System.out.println("## schema: " + schema);
+            // TODO ad-hoc
+            String path = task.getPaths().get(processorIndex);
+            //Schema schema = task.getSchema();
+            // TODO manually create schema object now
+            Schema schema = new Schema(Arrays.asList(
+                    new Column(0, "date_code", StringType.STRING),
+                    new Column(1, "customer_code", StringType.STRING),
+                    new Column(2, "product_code", StringType.STRING),
+                    new Column(3, "employee_code", StringType.STRING)));
 
             // TODO simple implementation
 
-            PageBuilder pageBuilder = new PageBuilder(bufferManager, schema, op);
+            PageBuilder pageBuilder = new PageBuilder(pageAllocator, schema, op);
             RecordBuilder recordBuilder = pageBuilder.builder();
-            recordBuilder.
-            Page page;
+
+            recordBuilder.startRecord();
+
+            // TODO execute it here?
             try (BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(path)))) {
                 String line;
-                int pos = 0;
-                page = Page.allocate(4096); // TODO use Page allocator or page builder?
                 while ((line = r.readLine()) != null) {
-                    System.out.println("# line: " + line);
-                    // TODO validate column size
-                    String[] columns = line.split(",");
-                    for (String column : columns) {
-                        byte[] bytes = column.getBytes();
-                        // TODO int:setBytes??
-                        page.setBytes(pos, bytes); // TODO charset?
-                        pos += bytes.length;
-                    }
+                    final String[] lineValues = line.split(","); // TODO ad-hoc parsing
+                    RecordProducer recordProducer = new RecordProducer() {
+                        @Override
+                        public void setLong(Column column, LongType.Setter setter) {
+                            // TODO setter.setLong(Long.parseLong(lineValues[column.getIndex()]));
+                        }
+
+                        @Override
+                        public void setDouble(Column column, DoubleType.Setter setter) {
+                            // TODO setter.setDouble(Double.parseDouble(lineValues[column.getIndex()]));
+                        }
+
+                        @Override
+                        public void setString(Column column, StringType.Setter setter) {
+                            setter.setString(lineValues[column.getIndex()]);
+                        }
+                    };
+                    schema.produce(recordBuilder, recordProducer);
+                    recordBuilder.addRecord();
                 }
             }
-            if (page != null) {
-                try {
-                    op.addPage(page);
-                } finally {
-                    page.clear();
-                }
-            }
+
+            recordBuilder.flush();
 
             return DynamicReport.builder();
         }
