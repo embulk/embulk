88deb29d25d269afb9afdf89a8b5e13072d8e703
Muga Nishizawa <muga.nishizawa@gmail.com>
2014-10-07 11:09:05 -0700

    ExecModule: minor changed

--- quickload-core/src/main/java/org/quickload/exec/ExecModule.java
+++ quickload-core/src/main/java/org/quickload/exec/ExecModule.java
@@ -1,12 +1,11 @@
 package org.quickload.exec;
 
 import com.google.inject.Module;
+import com.google.common.base.Preconditions;
 import com.google.inject.Binder;
 import com.google.inject.Scopes;
 import org.quickload.config.ModelManager;
 import org.quickload.record.TypeManager;
-
-import static com.google.common.base.Preconditions.checkNotNull;
 
 public class ExecModule
         implements Module
@@ -14,7 +13,7 @@ public class ExecModule
     @Override
     public void configure(Binder binder)
     {
-        checkNotNull(binder, "binder is null.");
+        Preconditions.checkNotNull(binder, "binder is null.");
         binder.bind(ModelManager.class).in(Scopes.SINGLETON);
         binder.bind(TypeManager.class).in(Scopes.SINGLETON);
     }
