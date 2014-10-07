f8cdea9134d72c22a08623069c601f6d0edb9eb7
Muga Nishizawa <muga.nishizawa@gmail.com>
2014-10-06 20:14:06 -0700

    ExecModule: inserted null check

--- quickload-core/src/main/java/org/quickload/exec/ExecModule.java
+++ quickload-core/src/main/java/org/quickload/exec/ExecModule.java
@@ -6,12 +6,15 @@ import com.google.inject.Scopes;
 import org.quickload.config.ModelManager;
 import org.quickload.record.TypeManager;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class ExecModule
         implements Module
 {
     @Override
     public void configure(Binder binder)
     {
+        checkNotNull(binder, "binder is null.");
         binder.bind(ModelManager.class).in(Scopes.SINGLETON);
         binder.bind(TypeManager.class).in(Scopes.SINGLETON);
     }
