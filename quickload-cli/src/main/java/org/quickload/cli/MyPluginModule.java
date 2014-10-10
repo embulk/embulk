c8e52b3da67067c09bd8c03e35f5621b25658ac5
Muga Nishizawa <muga.nishizawa@gmail.com>
2014-10-10 16:11:23 -0700

    MyPluginModule: renamed

--- quickload-cli/src/main/java/org/quickload/cli/MyPluginModule.java
+++ quickload-cli/src/main/java/org/quickload/cli/MyPluginModule.java
@@ -4,15 +4,21 @@ import com.google.common.base.Preconditions;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.name.Names;
+import org.quickload.in.LocalFileCsvInputPlugin;
+import org.quickload.out.LocalFileCsvOutputPlugin;
 import org.quickload.spi.CSVParserPlugin;
+import org.quickload.spi.InputPlugin;
+import org.quickload.spi.OutputPlugin;
 import org.quickload.spi.ParserPlugin;
 
-public class MyPluginSettingsModule implements Module
+public class MyPluginModule implements Module
 {
     @Override
     public void configure(Binder binder)
     {
         Preconditions.checkNotNull(binder, "binder is null.");
         binder.bind(ParserPlugin.class).annotatedWith(Names.named("my")).to(CSVParserPlugin.class);
+        binder.bind(InputPlugin.class).annotatedWith(Names.named("my")).to(LocalFileCsvInputPlugin.class);
+        binder.bind(OutputPlugin.class).annotatedWith(Names.named("my")).to(LocalFileCsvOutputPlugin.class);
     }
 }
