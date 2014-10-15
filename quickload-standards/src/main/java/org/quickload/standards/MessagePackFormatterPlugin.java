86d7183010cad134c3910eab0c147bcbeb1ecadc
Muga Nishizawa <muga.nishizawa@gmail.com>
2014-10-15 14:06:41 -0700

    MessagePackFormatterPlugin: prototyped

--- quickload-standards/src/main/java/org/quickload/standards/MessagePackFormatterPlugin.java
+++ quickload-standards/src/main/java/org/quickload/standards/MessagePackFormatterPlugin.java
@@ -1,15 +1,32 @@
 package org.quickload.standards;
 
+import com.google.inject.Inject;
+import org.msgpack.MessagePack;
+import org.msgpack.packer.BufferPacker;
+import org.msgpack.packer.Packer;
+import org.quickload.buffer.Buffer;
 import org.quickload.config.Task;
 import org.quickload.config.TaskSource;
 import org.quickload.config.ConfigSource;
-import org.quickload.record.Page;
-import org.quickload.record.Schema;
-import org.quickload.spi.*;
+import org.quickload.exec.BufferManager;
+import org.quickload.record.*;
+import org.quickload.spi.AbstractOperator;
+import org.quickload.spi.BufferOperator;
+import org.quickload.spi.FormatterPlugin;
+import org.quickload.spi.PageOperator;
+import org.quickload.spi.ProcTask;
+import org.quickload.spi.Report;
+
+import java.io.IOException;
 
 public class MessagePackFormatterPlugin
         implements FormatterPlugin
 {
+    private final BufferManager bufferManager;
+
+    @Inject
+    public MessagePackFormatterPlugin(BufferManager bufferManager) { this.bufferManager = bufferManager; }
+
     public interface PluginTask
             extends Task
     {
@@ -41,19 +58,89 @@ public class MessagePackFormatterPlugin
             implements PageOperator
     {
         private final Schema schema;
+        private final PageReader pageReader;
         private final int processorIndex;
+        private final MessagePack msgpack;
 
         private PluginOperator(Schema schema, int processorIndex, BufferOperator next)
         {
             super(next);
             this.schema = schema;
+            this.pageReader = new PageReader(bufferManager, schema);
             this.processorIndex = processorIndex;
+            this.msgpack = new MessagePack();
         }
 
         @Override
         public void addPage(Page page)
         {
-            // TODO
+            // TODO simple implementation
+            final BufferPacker packer = msgpack.createBufferPacker(); // TODO
+
+            RecordCursor recordCursor = pageReader.cursor(page);
+
+            while (recordCursor.next()) {
+                RecordConsumer recordConsumer = new RecordConsumer()
+                {
+                    @Override
+                    public void setNull(Column column)
+                    {
+                        try {
+                            writeColumnName(packer, column);
+                            packer.writeNil();
+                        } catch (IOException e) { // TOOD should handle IOException here?
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void setLong(Column column, long value)
+                    {
+                        try {
+                            writeColumnName(packer, column);
+                            packer.write(value);
+                        } catch (IOException e) { // TOOD should handle IOException here?
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void setDouble(Column column, double value) {
+                        try {
+                            writeColumnName(packer, column);
+                            packer.write(value);
+                        } catch (IOException e) { // TOOD should handle IOException here?
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void setString(Column column, String value) {
+                        try {
+                            writeColumnName(packer, column);
+                            packer.write(value);
+                        } catch (IOException e) { // TOOD should handle IOException here?
+                            e.printStackTrace();
+                        }
+                    }
+
+                    private void writeColumnName(Packer packer, Column column)
+                            throws IOException
+                    {
+                        packer.write(column.getName());
+                    }
+                };
+
+                try {
+                    packer.writeMapBegin(schema.getColumns().size());
+                    schema.consume(recordCursor, recordConsumer);
+                    packer.writeMapEnd();
+                } catch (IOException e) { // TODO better handling
+                    e.printStackTrace(); // TODO
+                }
+            }
+
+            next.addBuffer(new Buffer(packer.toByteArray()));
         }
 
         @Override
