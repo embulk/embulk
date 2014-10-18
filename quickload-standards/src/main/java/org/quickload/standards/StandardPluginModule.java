329a1b530a2c09406fc11fb706e81c6da8c60567
Muga Nishizawa <muga.nishizawa@gmail.com>
2014-10-17 23:51:43 -0700

    StandardPluginModule: moved

---
+++ quickload-standards/src/main/java/org/quickload/standards/StandardPluginModule.java
@@ -0,0 +1,34 @@
+package org.quickload.standards;
+
+import com.google.common.base.Preconditions;
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.name.Names;
+import org.quickload.spi.FormatterPlugin;
+import org.quickload.spi.InputPlugin;
+import org.quickload.spi.OutputPlugin;
+import org.quickload.spi.ParserPlugin;
+import org.quickload.standards.*;
+
+public class StandardPluginModule implements Module
+{
+    @Override
+    public void configure(Binder binder)
+    {
+        Preconditions.checkNotNull(binder, "binder is null.");
+
+        // input plugin
+        binder.bind(InputPlugin.class).annotatedWith(Names.named("local_file")).to(LocalFileInputPlugin.class);
+        binder.bind(InputPlugin.class).annotatedWith(Names.named("s3_file")).to(S3FileInputPlugin.class);
+
+        // output plugin
+        binder.bind(OutputPlugin.class).annotatedWith(Names.named("local_file")).to(LocalFileOutputPlugin.class);
+
+        // parser plugin
+        binder.bind(ParserPlugin.class).annotatedWith(Names.named("csv")).to(CsvParserPlugin.class);
+
+        // formatter plugin
+        binder.bind(FormatterPlugin.class).annotatedWith(Names.named("csv")).to(CsvFormatterPlugin.class);
+        binder.bind(FormatterPlugin.class).annotatedWith(Names.named("msgpack")).to(MessagePackFormatterPlugin.class);
+    }
+}
