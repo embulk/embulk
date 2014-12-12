682567ccdb82fa5cb53188896cd2e76249c9f7f0
Hiroshi Nakamura <nahi@ruby-lang.org>
2014-12-12 10:39:36 +0900

    Newline guess always returned CRLF
    String#count(chars) counts each char in a String and returns sum of
    those. Ashamed of quick fixing without test. I'll add after creating
    unit test environment.

--- lib/quickload/plugin/guess_newline.rb
+++ lib/quickload/plugin/guess_newline.rb
@@ -6,7 +6,7 @@ module QuickLoad::Plugin
     def guess_text(config, sample_text)
       cr_count = sample_text.count("\r")
       lf_count = sample_text.count("\n")
-      crlf_count = sample_text.count("\r\n")
+      crlf_count = sample_text.scan(/\r\n/).length
       if crlf_count > cr_count / 2 && crlf_count > lf_count / 2
         return {"newline" => "CRLF"}
       elsif cr_count > lf_count / 2
