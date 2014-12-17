11a1a027c0bec34d901f3fee532a221430c49a4d
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2014-12-16 17:45:05 -0800

    BufferManager.allocateBuffer allocates at least 128KB

--- embulk-core/src/main/java/org/embulk/exec/BufferManager.java
+++ embulk-core/src/main/java/org/embulk/exec/BufferManager.java
@@ -1,5 +1,7 @@
 package org.embulk.exec;
 
+import java.util.Deque;
+import java.util.ArrayDeque;
 import org.embulk.buffer.Buffer;
 import org.embulk.buffer.BufferAllocator;
 import org.embulk.record.Page;
@@ -8,16 +10,52 @@ import org.embulk.record.PageAllocator;
 public class BufferManager
         implements PageAllocator, BufferAllocator
 {
+    private final Deque<Buffer> buffers = new ArrayDeque<Buffer>();
+    private final Deque<Page> pages = new ArrayDeque<Page>();
+
     @Override
-    public Buffer allocateBuffer(int minimumCapacity)
+    public synchronized Buffer allocateBuffer(int minimumCapacity)
     {
-        return Buffer.allocate(minimumCapacity);
+        return Buffer.allocate(Math.max(128*1024, minimumCapacity));
+        /*
+        if (!buffers.isEmpty()) {
+            return buffers.removeFirst();
+        }
+        return new Buffer(Math.max(128*1024, minimumCapacity)) {
+            private boolean released = false;
+            public void release()
+            {
+                if (!released) {
+                    synchronized(BufferManager.this) {
+                        buffers.add(this);
+                    }
+                    released = true;
+                }
+            }
+        };
+        */
     }
 
     @Override
-    public Page allocatePage(int minimumCapacity)
+    public synchronized Page allocatePage(int minimumCapacity)
     {
-        // TODO cache
         return Page.allocate(Math.max(128*1024, minimumCapacity));
+        /*
+        if (!pages.isEmpty()) {
+            return pages.removeFirst();
+        }
+        return new Page(Math.max(128*1024, minimumCapacity)) {
+            private boolean released = false;
+            public void release()
+            {
+                if (!released) {
+                    synchronized(BufferManager.this) {
+                        pages.add(this);
+                    }
+                    released = true;
+                }
+            }
+        };
+        */
     }
 }
