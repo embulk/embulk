430cbe2e77a121b72e68dcff35440c9c08f0386b
Hiroshi Nakamura <nahi@ruby-lang.org>
2014-12-19 14:38:03 +0900

    fix: typo 'quickloa'

--- lib/embulk/bridge.rb
+++ lib/embulk/bridge.rb
@@ -1,6 +1,6 @@
 module Embulk
   module Bridge
-    require 'quickloa/java/imports'
+    require 'embulk/java/imports'
     require 'embulk/bridge/buffer'
     require 'embulk/bridge/data_source'
     require 'embulk/bridge/guess_plugin'
