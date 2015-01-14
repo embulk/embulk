471c37a4fa70f32d1f094b03434bb3d943fa5eb8
Hiroshi Nakamura <nahi@ruby-lang.org>
2015-01-14 21:23:43 +0900

    fix: restore TestPageBuilderReader

--- embulk-core/src/test/java/org/embulk/spi/TestPageBuilderReader.java
+++ embulk-core/src/test/java/org/embulk/spi/TestPageBuilderReader.java
@@ -1,20 +1,5 @@
 package org.embulk.spi;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.embulk.spi.BufferAllocator;
-import org.embulk.time.Timestamp;
-import org.embulk.type.Schema;
-import org.embulk.type.Column;
 import static org.embulk.spi.PageTestUtils.newColumn;
 import static org.embulk.spi.PageTestUtils.newSchema;
 import static org.embulk.type.Types.BOOLEAN;
@@ -22,23 +7,60 @@ import static org.embulk.type.Types.DOUBLE;
 import static org.embulk.type.Types.LONG;
 import static org.embulk.type.Types.STRING;
 import static org.embulk.type.Types.TIMESTAMP;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.embulk.EmbulkTestRuntime;
+import org.embulk.time.Timestamp;
+import org.embulk.type.Schema;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 public class TestPageBuilderReader
 {
-    /* TODO
     @Rule
     public EmbulkTestRuntime runtime = new EmbulkTestRuntime();
 
-    private BufferAllocator bufferAllocator;
+    public static class MockPageOutput implements PageOutput
+    {
+        public List<Page> pages;
 
+        public MockPageOutput()
+        {
+            this.pages = new ArrayList<>();
+        }
+
+        @Override
+        public void add(Page page)
+        {
+            pages.add(page);
+        }
+
+        @Override
+        public void finish()
+        {
+        }
+
+        @Override
+        public void close()
+        {
+        }
+    }
+
+    private BufferAllocator bufferAllocator;
     private PageReader reader;
     private PageBuilder builder;
 
     @Before
     public void setup()
     {
-        this.bufferAllocator = runtime.getInstance(BufferAllocator.class);
+        this.bufferAllocator = runtime.getBufferAllocator();
     }
 
     @After
@@ -57,200 +79,155 @@ public class TestPageBuilderReader
     @Test
     public void testBoolean()
     {
-        check(newSchema(newColumn("col1", BOOLEAN)),
-                false, true, true);
+        check(newSchema(newColumn("col1", BOOLEAN)), false, true, true);
     }
 
     @Test
     public void testLong()
     {
-        check(newSchema(newColumn("col1", LONG)),
-                1L, Long.MIN_VALUE, Long.MAX_VALUE);
+        check(newSchema(newColumn("col1", LONG)), 1L, Long.MIN_VALUE,
+                Long.MAX_VALUE);
     }
 
     @Test
     public void testDouble()
     {
-        check(newSchema(newColumn("col1", DOUBLE)),
-                8.1, 3.141592, 4.3);
+        check(newSchema(newColumn("col1", DOUBLE)), 8.1, 3.141592, 4.3);
     }
 
     @Test
     public void testUniqueStrings()
     {
-        check(newSchema(newColumn("col1", STRING)),
-                "test1", "test2", "test0");
+        check(newSchema(newColumn("col1", STRING)), "test1", "test2", "test0");
     }
 
     @Test
     public void testDuplicateStrings()
     {
-        check(newSchema(newColumn("col1", STRING)),
-                "test1", "test1", "test1");
+        check(newSchema(newColumn("col1", STRING)), "test1", "test1", "test1");
     }
 
     @Test
     public void testDuplicateStringsMultiColumns()
     {
         check(newSchema(newColumn("col1", STRING), newColumn("col1", STRING)),
-                "test2", "test1",
-                "test1", "test2",
-                "test2", "test0",
-                "test1", "test1");
+                "test2", "test1", "test1", "test2", "test2", "test0", "test1",
+                "test1");
     }
 
     @Test
     public void testTimestamp()
     {
         check(newSchema(newColumn("col1", TIMESTAMP)),
-                Timestamp.ofEpochMilli(0),
-                Timestamp.ofEpochMilli(10));
+                Timestamp.ofEpochMilli(0), Timestamp.ofEpochMilli(10));
     }
 
     @Test
     public void testNull()
     {
-        check(newSchema(
-                    newColumn("col3", DOUBLE),
-                    newColumn("col1", STRING),
-                    newColumn("col3", LONG),
-                    newColumn("col3", BOOLEAN),
-                    newColumn("col2", TIMESTAMP)),
-                null, null, null, null, null,
-                8122.0, "val1", 3L, false, Timestamp.ofEpochMilli(0),
+        check(newSchema(newColumn("col3", DOUBLE), newColumn("col1", STRING),
+                newColumn("col3", LONG), newColumn("col3", BOOLEAN),
+                newColumn("col2", TIMESTAMP)), null, null, null, null, null,
                 null, null, null, null, null);
     }
 
     @Test
     public void testMixedTypes()
     {
-        check(newSchema(
-                    newColumn("col3", DOUBLE),
-                    newColumn("col1", STRING),
-                    newColumn("col3", LONG),
-                    newColumn("col3", BOOLEAN),
-                    newColumn("col2", TIMESTAMP)),
-                8122.0, "val1", 3L, false, Timestamp.ofEpochMilli(0),
-                140.15, "val2", Long.MAX_VALUE, true, Timestamp.ofEpochMilli(10));
+        check(newSchema(newColumn("col3", DOUBLE), newColumn("col1", STRING),
+                newColumn("col3", LONG), newColumn("col3", BOOLEAN),
+                newColumn("col2", TIMESTAMP)), 8122.0, "val1", 3L, false,
+                Timestamp.ofEpochMilli(0), 140.15, "val2", Long.MAX_VALUE,
+                true, Timestamp.ofEpochMilli(10));
     }
 
     private void check(Schema schema, Object... objects)
     {
-        buildPage(schema, objects);
-        checkPage(schema, objects);
+        Page page = buildPage(schema, objects);
+        checkPage(schema, page, objects);
     }
 
-    private void buildPage(Schema schema, final Object... objects)
+    private Page buildPage(Schema schema, final Object... objects)
     {
-        this.builder = new PageBuilder(bufferAllocator, schema, channel.getOutput());
-        final AtomicInteger i = new AtomicInteger(0);
-        while (i.get() < objects.length) {
-            builder.addRecord(new SchemaVisitor() {
-                public void writeBoolean(Column column, BooleanWriter writer)
-                {
-                    if (objects[i.get()] == null) {
-                        writer.writeNull();
-                    } else {
-                        writer.write((boolean) objects[i.get()]);
-                    }
-                    i.getAndIncrement();
-                }
+        List<Page> pages = buildPages(schema, objects);
+        assertEquals(1, pages.size());
+        return pages.get(0);
+    }
 
-                public void writeLong(Column column, LongWriter writer)
-                {
-                    if (objects[i.get()] == null) {
-                        writer.writeNull();
-                    } else {
-                        writer.write((long) objects[i.get()]);
-                    }
-                    i.getAndIncrement();
+    private List<Page> buildPages(Schema schema, final Object... objects)
+    {
+        MockPageOutput output = new MockPageOutput();
+        this.builder = new PageBuilder(bufferAllocator, schema, output);
+        int idx = 0;
+        while (idx < objects.length) {
+            for (int column = 0; column < builder.getSchema().getColumnCount(); ++column) {
+                Object value = objects[idx++];
+                if (value == null) {
+                    builder.setNull(column);
+                } else if (value instanceof Boolean) {
+                    builder.setBoolean(column, (Boolean) value);
+                } else if (value instanceof Double) {
+                    builder.setDouble(column, (Double) value);
+                } else if (value instanceof Long) {
+                    builder.setLong(column, (Long) value);
+                } else if (value instanceof String) {
+                    builder.setString(column, (String) value);
+                } else if (value instanceof Timestamp) {
+                    builder.setTimestamp(column, (Timestamp) value);
+                } else {
+                    throw new IllegalStateException(
+                            "Unsupported type in test utils: "
+                                    + value.toString());
                 }
-
-                public void writeDouble(Column column, DoubleWriter writer)
-                {
-                    if (objects[i.get()] == null) {
-                        writer.writeNull();
-                    } else {
-                        writer.write((double) objects[i.get()]);
-                    }
-                    i.getAndIncrement();
-                }
-
-                public void writeString(Column column, StringWriter writer)
-                {
-                    if (objects[i.get()] == null) {
-                        writer.writeNull();
-                    } else {
-                        writer.write((String) objects[i.get()]);
-                    }
-                    i.getAndIncrement();
-                }
-
-                public void writeTimestamp(Column column, TimestampWriter writer)
-                {
-                    if (objects[i.get()] == null) {
-                        writer.writeNull();
-                    } else {
-                        writer.write((Timestamp) objects[i.get()]);
-                    }
-                    i.getAndIncrement();
-                }
-            });
+            }
+            builder.addRecord();
         }
+        builder.flush();
         builder.close();
-        channel.completeProducer();
+        return output.pages;
     }
 
-    private void checkPage(Schema schema, final Object... objects)
+    private void checkPage(Schema schema, Page page, final Object... objects)
     {
-        this.reader = new PageReader(schema, channel.getInput());
-        final AtomicInteger i = new AtomicInteger(0);
-        while (reader.nextRecord()) {
-            reader.visitColumns(new SchemaVisitor() {
-                public void readNull(Column column)
-                {
-                    assertNull(objects[i.getAndIncrement()]);
+        this.reader = new PageReader(schema);
+        reader.setPage(page);
+        int idx = 0;
+        while (idx < objects.length && reader.nextRecord()) {
+            for (int column = 0; column < reader.getSchema().getColumnCount(); ++column) {
+                Object value = objects[idx++];
+                if (value == null) {
+                    assertEquals(true, reader.isNull(column));
+                } else if (value instanceof Boolean) {
+                    assertEquals(value, reader.getBoolean(column));
+                } else if (value instanceof Double) {
+                    assertEquals(value, reader.getDouble(column));
+                } else if (value instanceof Long) {
+                    assertEquals(value, reader.getLong(column));
+                } else if (value instanceof String) {
+                    assertEquals(value, reader.getString(column));
+                } else if (value instanceof Timestamp) {
+                    assertEquals(value, reader.getTimestamp(column));
+                } else {
+                    throw new IllegalStateException(
+                            "Unsupported type in test utils: "
+                                    + value.toString());
                 }
-
-                public void readBoolean(Column column, boolean value)
-                {
-                    assertEquals(objects[i.getAndIncrement()], value);
-                }
-
-                public void readLong(Column column, long value)
-                {
-                    assertEquals(objects[i.getAndIncrement()], value);
-                }
-
-                public void readDouble(Column column, double value)
-                {
-                    assertNotNull(objects[i.get()]);
-                    assertEquals((double) objects[i.getAndIncrement()], value, 0.00001);
-                }
-
-                public void readString(Column column, String value)
-                {
-                    assertEquals(objects[i.getAndIncrement()], value);
-                }
-
-                public void readTimestamp(Column column, Timestamp value)
-                {
-                    assertEquals(objects[i.getAndIncrement()], value);
-                }
-            });
+            }
         }
-        assertEquals(objects.length, i.get());
     }
 
     @Test
     public void testEmptySchema()
     {
-        this.builder = new PageBuilder(bufferAllocator, newSchema(), channel.getOutput());
+        MockPageOutput output = new MockPageOutput();
+        this.builder = new PageBuilder(bufferAllocator, newSchema(), output);
         builder.addRecord();
         builder.addRecord();
+        builder.flush();
         builder.close();
-        channel.completeProducer();
-        this.reader = new PageReader(newSchema(), channel.getInput());
+        this.reader = new PageReader(newSchema());
+        assertEquals(1, output.pages.size());
+        reader.setPage(output.pages.get(0));
         assertTrue(reader.nextRecord());
         assertTrue(reader.nextRecord());
         assertFalse(reader.nextRecord());
@@ -259,55 +236,68 @@ public class TestPageBuilderReader
     @Test
     public void testRenewPage()
     {
-        this.bufferAllocator = new bufferAllocator() {
-            public Page allocatePage(int minimumCapacity)
+        this.bufferAllocator = new BufferAllocator()
+        {
+            @Override
+            public Buffer allocate()
             {
-                return Page.allocate(minimumCapacity);
+                return Buffer.allocate(1);
+            }
+
+            @Override
+            public Buffer allocate(int minimumCapacity)
+            {
+                return Buffer.allocate(minimumCapacity);
             }
         };
-        check(newSchema(newColumn("col1", LONG)),
-                0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L);
+        assertEquals(
+                9,
+                buildPages(newSchema(newColumn("col1", LONG)), 0L, 1L, 2L, 3L,
+                        4L, 5L, 6L, 7L, 8L).size());
     }
 
     @Test
     public void testRenewPageWithStrings()
     {
-        this.bufferAllocator = new bufferAllocator() {
-            public Page allocatePage(int minimumCapacity)
+        this.bufferAllocator = new BufferAllocator()
+        {
+            @Override
+            public Buffer allocate()
             {
-                return Page.allocate(minimumCapacity + 3);
+                return Buffer.allocate(1);
+            }
+
+            @Override
+            public Buffer allocate(int minimumCapacity)
+            {
+                return Buffer.allocate(minimumCapacity);
             }
         };
-        check(newSchema(
-                    newColumn("col1", LONG),
-                    newColumn("col1", STRING)),
-                0L, "record0",
-                1L, "record1",
-                3L, "record3");
+        assertEquals(
+                3,
+                buildPages(
+                        newSchema(newColumn("col1", LONG),
+                                newColumn("col1", STRING)), 0L, "record0", 1L,
+                        "record1", 3L, "record3").size());
     }
 
     @Test
     public void testRepeatableClose()
     {
-        this.builder = new PageBuilder(bufferAllocator, newSchema(newColumn("col1", STRING)), channel.getOutput());
+        MockPageOutput output = new MockPageOutput();
+        this.builder = new PageBuilder(bufferAllocator, newSchema(newColumn(
+                "col1", STRING)), output);
         builder.close();
         builder.close();
-        channel.completeProducer();
-        for (Page page : channel.getInput()) {
-            fail();
-        }
     }
 
     @Test
     public void testRepeatableFlush()
     {
-        this.builder = new PageBuilder(bufferAllocator, newSchema(newColumn("col1", STRING)), channel.getOutput());
+        MockPageOutput output = new MockPageOutput();
+        this.builder = new PageBuilder(bufferAllocator, newSchema(newColumn(
+                "col1", STRING)), output);
         builder.flush();
         builder.flush();
-        channel.completeProducer();
-        for (Page page : channel.getInput()) {
-            fail();
-        }
     }
-    */
 }
