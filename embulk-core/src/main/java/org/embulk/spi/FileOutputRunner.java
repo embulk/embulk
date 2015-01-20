1a4f5122fbd49a8402ec5188dc86fab80f3ca66d
Hiroshi Nakamura <nahi@ruby-lang.org>
2015-01-20 21:22:18 +0900

    findbugs: remove unused local variable

--- embulk-core/src/main/java/org/embulk/spi/FileOutputRunner.java
+++ embulk-core/src/main/java/org/embulk/spi/FileOutputRunner.java
@@ -2,7 +2,6 @@ package org.embulk.spi;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 import org.embulk.config.Task;
 import org.embulk.config.TaskSource;
 import org.embulk.config.ConfigSource;
@@ -127,7 +126,6 @@ public class FileOutputRunner
     {
         private final Transactional tran;
         private final PageOutput output;
-        private boolean finished;
 
         public DelegateTransactionalPageOutput(Transactional tran, PageOutput output)
         {
@@ -145,7 +143,6 @@ public class FileOutputRunner
         public void finish()
         {
             output.finish();
-            finished = true;
         }
 
         @Override
