e66be2ac33582cc420d5c7e81f44e8f12a79b756
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2015-01-27 15:58:01 +0900

    guess_gzip doesn't need Guess module

--- lib/embulk/guess_gzip.rb
+++ lib/embulk/guess_gzip.rb
@@ -1,18 +1,16 @@
 module Embulk
-  module Guess
 
-    class GzipGuess < GuessPlugin
-      Plugin.register_guess('gzip', self)
+  class GzipGuess < GuessPlugin
+    Plugin.register_guess('gzip', self)
 
-      GZIP_HEADER = "\x1f\x8b".force_encoding('ASCII-8BIT').freeze
+    GZIP_HEADER = "\x1f\x8b".force_encoding('ASCII-8BIT').freeze
 
-      def guess(config, sample_buffer)
-        if sample_buffer[0,2] == GZIP_HEADER
-          return {"decoders" => [{"type" => "gzip"}]}
-        end
-        return {}
+    def guess(config, sample_buffer)
+      if sample_buffer[0,2] == GZIP_HEADER
+        return {"decoders" => [{"type" => "gzip"}]}
       end
+      return {}
     end
-
   end
+
 end
