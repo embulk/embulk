1fdd3e28568e41f2c3721c1df786359519d408bb
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2015-02-13 14:59:43 -0800

    added missing ConfigInject.java

---
+++ embulk-core/src/main/java/org/embulk/config/ConfigInject.java
@@ -0,0 +1,14 @@
+package org.embulk.config;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ConfigInject
+{
+}
