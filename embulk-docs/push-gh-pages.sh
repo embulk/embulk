3f22fceeb81de67417fa49c9ed8b622f03831fbc
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2015-02-13 16:54:58 -0800

    fixed push-gh-pages

--- embulk-docs/push-gh-pages.sh
+++ embulk-docs/push-gh-pages.sh
@@ -3,14 +3,18 @@
 set -e
 [ "$TRAVIS_PULL_REQUEST" != "false" ] && exit 0
 
-revision="`git rev-parse HEAD`"
+revision="$(git rev-parse HEAD)"
+remote="$(git config remote.origin.url | sed "s+^git:+http://+")"
 ./gradlew site
 
 rm -rf gh_pages
 git clone . gh_pages
 cd gh_pages
 
-git checkout gh-pages
+git remote add travis_push "$remote"
+git fetch travis_push
+
+git checkout -b gh-pages travis_push/gh-pages
 rm -rf docs
 cp -a ../embulk-docs/build/html docs
 git add docs
@@ -22,4 +26,4 @@ git commit -m "Updated document $revision" || exit 0
 git config credential.helper "store --file=.git_credential_helper"
 echo "https://$GITHUB_TOKEN:@github.com" > "$HOME/.git_credential_helper"
 trap "rm -rf $HOME/.git_credential_helper" EXIT
-git push origin gh-pages
+git push travis_push gh-pages
