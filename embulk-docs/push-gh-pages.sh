fd7b0701e75770ec98f246cd35b19974bc06454d
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2015-02-15 17:44:25 -0800

    push-gh-pages: don't use 'set -e' which seems not taking effect

--- embulk-docs/push-gh-pages.sh
+++ embulk-docs/push-gh-pages.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
-set -e
-[ "$TRAVIS_PULL_REQUEST" != "false" ] && exit 0
+function re() {
+    r "$@"
+    if [ $? -ne 0 ];then
+        exit $?
+    fi
+}
+
+function r() {
+    echo "$@"
+    "$@"
+}
+
+#[ "$TRAVIS_PULL_REQUEST" != "false" ] && exit 0
 
 revision="$(git rev-parse HEAD)"
 remote="$(git config remote.origin.url | sed "s+^git:+http://+")"
-./gradlew site
+re ./gradlew site
 
-git fetch --unshallow || echo "using complete repository."
+r git fetch --unshallow || echo "using complete repository."
 
-rm -rf gh_pages
-git clone . gh_pages
-cd gh_pages
+re rm -rf gh_pages
+re git clone . gh_pages
+re cd gh_pages
 
-git remote add travis_push "$remote"
-git fetch travis_push
+re git remote add travis_push "$remote"
+re git fetch travis_push
 
-git checkout -b gh-pages travis_push/gh-pages
-rm -rf docs
-cp -a ../embulk-docs/build/html docs
-git add docs
+re git checkout -b gh-pages travis_push/gh-pages
+re rm -rf docs
+re cp -a ../embulk-docs/build/html docs
+re git add docs
 
-git config user.name "$GIT_USER_NAME"
-git config user.email "$GIT_USER_EMAIL"
-git commit -m "Updated document $revision" || exit 0
+re git config user.name "$GIT_USER_NAME"
+re git config user.email "$GIT_USER_EMAIL"
+r git commit -m "Updated document $revision"
 
-git config credential.helper "store --file=.git_credential_helper"
+re git config credential.helper "store --file=.git_credential_helper"
 echo "https://$GITHUB_TOKEN:@github.com" > "$HOME/.git_credential_helper"
 trap "rm -rf $HOME/.git_credential_helper" EXIT
-git push travis_push gh-pages
+#re git push travis_push gh-pages
