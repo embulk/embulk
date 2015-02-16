ed0eb1dbaa9dd3222d506076890e771fe75842a1
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2015-02-16 08:46:00 -0800

    fixed namespace of guess plugins

--- lib/embulk/guess/charset.rb
+++ lib/embulk/guess/charset.rb
@@ -1,26 +1,28 @@
 module Embulk
+  module Guess
 
-  class GuessCharset < GuessPlugin
-    Plugin.register_guess('charset', self)
+    class CharsetGuessPlugin < GuessPlugin
+      Plugin.register_guess('charset', self)
 
-    def guess(config, sample_buffer)
-      # ICU4J
-      detector = com.ibm.icu.text.CharsetDetector.new
-      detector.setText(sample_buffer.to_java_bytes)
-      best_match = detector.detect
-      if best_match.getConfidence < 50
-        name = "UTF-8"
-      else
-        name = best_match.getName
-        if name == "ISO-8859-1"
-          # ISO-8859-1 means ASCII which is a subset
-          # of UTF-8 in most of cases due to lack of
-          # sample data set
+      def guess(config, sample_buffer)
+        # ICU4J
+        detector = com.ibm.icu.text.CharsetDetector.new
+        detector.setText(sample_buffer.to_java_bytes)
+        best_match = detector.detect
+        if best_match.getConfidence < 50
           name = "UTF-8"
+        else
+          name = best_match.getName
+          if name == "ISO-8859-1"
+            # ISO-8859-1 means ASCII which is a subset
+            # of UTF-8 in most of cases due to lack of
+            # sample data set
+            name = "UTF-8"
+          end
         end
+        return {"parser" => {"charset" => name}}
       end
-      return {"parser" => {"charset" => name}}
     end
-  end
 
+  end
 end
--- lib/embulk/guess/newline.rb
+++ lib/embulk/guess/newline.rb
@@ -1,20 +1,22 @@
 module Embulk
+  module Guess
 
-  class GuessNewline < TextGuessPlugin
-    Plugin.register_guess('newline', self)
+    class NewlineGuessPlugin < TextGuessPlugin
+      Plugin.register_guess('newline', self)
 
-    def guess_text(config, sample_text)
-      cr_count = sample_text.count("\r")
-      lf_count = sample_text.count("\n")
-      crlf_count = sample_text.scan(/\r\n/).length
-      if crlf_count > cr_count / 2 && crlf_count > lf_count / 2
-        return {"parser" => {"newline" => "CRLF"}}
-      elsif cr_count > lf_count / 2
-        return {"parser" => {"newline" => "CR"}}
-      else
-        return {"parser" => {"newline" => "LF"}}
+      def guess_text(config, sample_text)
+        cr_count = sample_text.count("\r")
+        lf_count = sample_text.count("\n")
+        crlf_count = sample_text.scan(/\r\n/).length
+        if crlf_count > cr_count / 2 && crlf_count > lf_count / 2
+          return {"parser" => {"newline" => "CRLF"}}
+        elsif cr_count > lf_count / 2
+          return {"parser" => {"newline" => "CR"}}
+        else
+          return {"parser" => {"newline" => "LF"}}
+        end
       end
     end
-  end
 
+  end
 end
