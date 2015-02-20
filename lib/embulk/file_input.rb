8a824058f6ff5169823706c142ad4b04a4a131dc
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2015-02-19 18:11:18 -0800

    added IO interface to Embulk::FileInput

--- lib/embulk/file_input.rb
+++ lib/embulk/file_input.rb
@@ -5,6 +5,7 @@ module Embulk
   class FileInput
     def initialize(java_file_input)
       @java_file_input = java_file_input
+      @buffer = nil
     end
 
     def next_file
@@ -16,11 +17,63 @@ module Embulk
     end
 
     def each(&block)
+      if @buffer
+        yield @buffer
+        @buffer = nil
+      end
+
       while java_buffer = @java_file_input.poll
         buffer = Buffer.from_java(java_buffer)
         java_buffer.release
         yield buffer
       end
+    end
+
+    def read(count=nil, dest=nil)
+      if count == nil
+        @buffer ||= Buffer.new
+        while java_buffer = @java_file_input.poll
+          @buffer << Buffer.from_java(java_buffer)
+          java_buffer.release
+        end
+
+        return nil if @buffer.empty? && count != 0
+
+        if dest
+          dest.replace(@buffer)
+        else
+          dest = @buffer
+        end
+        @buffer = nil
+
+      else
+        @buffer ||= Buffer.new
+        until @buffer.size >= count
+          java_buffer = @java_file_input.poll
+          break unless java_buffer
+          @buffer << Buffer.from_java(java_buffer)
+          java_buffer.release
+        end
+
+        return nil if @buffer.empty? && count != 0
+
+        if @buffer.size <= count
+          if dest
+            dest.replace(@buffer)
+          else
+            dest = @buffer
+          end
+          @buffer = nil
+        else
+          data = @buffer.slice!(0, count)
+          if dest
+            dest.replace(data)
+          else
+            dest = data
+          end
+        end
+      end
+      return dest
     end
 
     def close
