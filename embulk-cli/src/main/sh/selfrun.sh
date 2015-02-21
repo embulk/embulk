38b000d5c6be13ad4ea2818d3cf6ea67e9d86c51
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2015-02-20 21:33:03 -0800

    updated the self-run script attached to the beggening of the jar file

---
+++ embulk-cli/src/main/sh/selfrun.sh
@@ -0,0 +1,64 @@
+
+: <<BAT
+@echo off
+
+java -jar %0 %*
+
+exit /B
+BAT
+
+java_args=""
+jruby_args=""
+default_optimize=""
+overwrite_optimize=""
+
+while true; do
+    case "$1" in
+        "-J+O")
+            overwrite_optimize="true"
+            shift
+            break;
+            ;;
+        "-J-O")
+            overwrite_optimize="false"
+            shift
+            break;
+            ;;
+        -J*)
+            v="${1#-J}"
+            if test "$v"; then
+                java_args="$java_args $v"
+            else
+                shift
+                file_args=`cat "$1"`
+                if test $? -ne 0; then
+                    echo "Failed to load java argument file."
+                    exit 1
+                fi
+                java_args="$java_args $file_args"
+            fi
+            shift
+            ;;
+        -R*)
+            v="${1#-R}"
+            jruby_args="$jruby_args $v"
+            shift
+            ;;
+        run)
+            default_optimize="true"
+            break
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if test "$overwrite_optimize" = "true" -o "$default_optimize" -a "$overwrite_optimize" != "false"; then
+    java_args="-XX:+AggressiveOpts -XX:+UseConcMarkSweepGC $java_args"
+else
+    java_args="-XX:+AggressiveOpts -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none $java_args"
+fi
+
+exec java $java_args -jar "$0" $jruby_args "$@"
+exit 127
