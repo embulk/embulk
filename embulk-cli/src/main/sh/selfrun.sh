9a30e8165a7311f2dbce4e386bdde8e064cd0f6c
Genya Hatakeyama <gennya@gmail.com>
2015-03-15 17:27:20 +0900

    add win32 batch(WIP)

--- embulk-cli/src/main/sh/selfrun.sh
+++ embulk-cli/src/main/sh/selfrun.sh
@@ -1,64 +1,75 @@
+@echo on
+setlocal enabledelayedexpansion
 
-: <<BAT
-@echo off
+SET java_args=
+SET jruby_args=
+SET default_optimize=0
+SET overwrite_optimize=0
+SET file_args=
+SET FILE_FLAG=0
 
-java -jar %~f0 %*
+if "%1" == "" goto :OPT_END
+
+:OPT_START
+rem Get args
+SET ARGS=%1
+rem Argument is to empty if OPT_END
+if "%ARGS%"== "" goto :OPT_END
+
+rem java wo/optimization
+if /i "%ARGS%" == "run" (
+    SET default_optimize=1
+    goto SHIFT
+)
+
+echo %FILE_FLAG%
+if %FILE_FLAG% == 1 (
+  rem for /f %%i in ("%ARGS%") do SET file_args=%%i
+  SET /P file_args=<"%ARGS%"
+  SET java_args=%java_args% %file_args%
+  SET FILE_FLAG=0
+  goto SHIFT
+)
+
+SET OPTION=%ARGS:~0,2%
+SET OPTION_ARG=!ARGS:%OPTION%=!
+if /i "%OPTION%" == "-J" (
+  if /i "%OPTION_ARG%" == "+O" (
+    SET overwrite_optimize=1
+    goto SHIFT
+  )
+  if /i "%OPTION_ARG%" == "-O" (
+    SET overwrite_optimize=0
+    goto SHIFT
+  )
+  rem java flag(file)
+  if "%OPTION_ARG%" == "" (
+    SET FILE_FLAG=1
+    goto SHIFT
+  )
+  
+  SET java_args=%OPTION_ARG%
+  goto SHIFT
+)
+
+rem Other *)
+goto :OPT_END
+
+:SHIFT
+shift /1
+goto :OPT_START
+
+:OPT_END
+
+SET FLAG=FALSE
+if %overwrite_optimize% == 1 SET FLAG=TRUE
+if %default_optimize% == 1 SET FLAG=TRUE
+if %FLAG% == TRUE (
+  SET java_args=-XX:+AggressiveOpts -XX:+UseConcMarkSweepGC %java_args%
+) ELSE (
+  SET java_args=-XX:+AggressiveOpts -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none %java_args%
+)
+
+echo java %java_args% -jar %~0 %jruby_args% %*
 
 exit /B
-BAT
-
-java_args=""
-jruby_args=""
-default_optimize=""
-overwrite_optimize=""
-
-while true; do
-    case "$1" in
-        "-J+O")
-            overwrite_optimize="true"
-            shift
-            break;
-            ;;
-        "-J-O")
-            overwrite_optimize="false"
-            shift
-            break;
-            ;;
-        -J*)
-            v="${1#-J}"
-            if test "$v"; then
-                java_args="$java_args $v"
-            else
-                shift
-                file_args=`cat "$1"`
-                if test $? -ne 0; then
-                    echo "Failed to load java argument file."
-                    exit 1
-                fi
-                java_args="$java_args $file_args"
-            fi
-            shift
-            ;;
-        -R*)
-            v="${1#-R}"
-            jruby_args="$jruby_args $v"
-            shift
-            ;;
-        run)
-            default_optimize="true"
-            break
-            ;;
-        *)
-            break
-            ;;
-    esac
-done
-
-if test "$overwrite_optimize" = "true" -o "$default_optimize" -a "$overwrite_optimize" != "false"; then
-    java_args="-XX:+AggressiveOpts -XX:+UseConcMarkSweepGC $java_args"
-else
-    java_args="-XX:+AggressiveOpts -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none $java_args"
-fi
-
-exec java $java_args -jar "$0" $jruby_args "$@"
-exit 127
