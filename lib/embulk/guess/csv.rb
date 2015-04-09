dfee99a555245b7c3be52715e1282339aa7c5d8a
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-04-09 11:25:21 -0700

    csv parser supports allow_extra_columns option to control whether skip or not rows with too many columns

--- lib/embulk/guess/csv.rb
+++ lib/embulk/guess/csv.rb
@@ -75,6 +75,9 @@ module Embulk
           parser_guessed["skip_header_lines"] = skip_header_lines
         end
 
+        parser_guessed["allow_extra_columns"] = false
+        parser_guessed["allow_optional_columns"] = false
+
         unless parser_config.has_key?("columns")
           if header_line
             column_names = sample_records.first
