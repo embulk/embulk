2619e76c7c0781aa243679470569ac1d1313f205
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-04-16 18:11:05 -0700

    travis.yml runs rubyTest

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,12 @@
 require 'test/unit'
 
+classpath_dir = File.expand_path('../classpath', File.dirname(__FILE__))
+jars = Dir.entries(classpath_dir).select {|f| f =~ /\.jar$/ }.sort
+jars.each do |jar|
+  require File.join(classpath_dir, jar)
+end
+
+require 'embulk/java/bootstrap'
 require 'embulk'
 
 # TODO simplecov
