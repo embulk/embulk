3293948d1e3d0f8e005b310ef03979167c48213f
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-04-19 12:32:40 -0700

    PluginManager doesn't unwrap PluginSourceNotMatchException

--- embulk-core/src/main/java/org/embulk/plugin/PluginManager.java
+++ embulk-core/src/main/java/org/embulk/plugin/PluginManager.java
@@ -13,7 +13,7 @@ public class PluginManager
     private final List<PluginSource> sources;
     private final Injector injector;
 
-    // Set<PluginSource> is injected BuiltinPluginSourceModule or extensions
+    // Set<PluginSource> is injected by BuiltinPluginSourceModule or extensions
     // using Multibinder<PluginSource>.
     @Inject
     public PluginManager(Set<PluginSource> pluginSources, Injector injector)
@@ -32,31 +32,34 @@ public class PluginManager
             throw new ConfigException(String.format("%s type is not set (if you intend to use NullOutputPlugin, you should enclose null in quotes such as {type: \"null\"}.", iface.getSimpleName()));
         }
 
-        List<Throwable> causes = new ArrayList<Throwable>();
+        List<PluginSourceNotMatchException> exceptions = new ArrayList<>();
         for (PluginSource source : sources) {
             try {
                 return source.newPlugin(iface, type);
             } catch (PluginSourceNotMatchException e) {
-                if (e.getCause() != null) {
-                    causes.add(e.getCause());
-                } else {
-                    causes.add(e);
-                }
+                exceptions.add(e);
             }
         }
 
+        throw buildPluginNotFoundException(iface, type, exceptions);
+    }
+
+    private static ConfigException buildPluginNotFoundException(Class<?> iface, PluginType type,
+            List<PluginSourceNotMatchException> exceptions)
+    {
         StringBuilder message = new StringBuilder();
         message.append(String.format("%s '%s' is not found.", iface.getSimpleName(), type.getName()));
-        for (Throwable cause : causes) {
+        for (PluginSourceNotMatchException exception : exceptions) {
+            Throwable cause = (exception.getCause() == null ? exception : exception.getCause());
             if (cause.getMessage() != null) {
                 message.append(String.format("%n"));
                 message.append(cause.getMessage());
             }
         }
         ConfigException e = new ConfigException(message.toString());
-        for (Throwable cause : causes) {
-            e.addSuppressed(cause);
+        for (PluginSourceNotMatchException exception : exceptions) {
+            e.addSuppressed(exception);
         }
-        throw e;
+        return e;
     }
 }
