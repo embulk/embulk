8ce37d2d7d4ab430c077c201a184d4c0105ee81c
hito4t <thitoshi@cac.co.jp>
2015-05-29 12:00:50 +0900

    Implemeted selfrun.sh for Windows

--- embulk-cli/src/main/sh/selfrun.sh
+++ embulk-cli/src/main/sh/selfrun.sh
@@ -2,7 +2,91 @@
 : <<BAT
 @echo off
 
-java -jar %~f0 %*
+setlocal
+
+set this=%0
+set java_args=
+set jruby_args=
+set default_optimize=
+set overwrite_optimize=
+
+:loop
+    set temp=%~1
+    if "%temp%" == "-J+O" (
+        set overwrite_optimize=true
+        shift
+        goto end
+        
+    ) else if "%temp%" == "-J-O" (
+        set overwrite_optimize=false
+        shift
+        goto end
+        
+    ) else if "%temp:~0,2%" == "-J" (
+        if not "%temp:~2%" == "" (
+            set java_args=%java_args% %temp:~2%
+        ) else (
+            if not exist "%~2" (
+                echo "failed to load java argument file."
+                exit /b 1
+            )
+            goto read
+        )
+        shift
+        goto loop
+        
+    ) else if "%temp:~0,2%" == "-R" (
+        set jruby_args=%jruby_args% %temp:~2%
+        shift
+        goto loop
+        
+    ) else if "%temp%" == "run" (
+        set default_optimize=true
+        goto end
+        
+    ) else (
+        goto end
+    )
+    
+:read
+
+for /f "delims=" %%i in (%~2) do set java_args=%java_args% %%i
+shift
+shift
+
+goto loop
+
+:end
+
+rem "%*" is not changed by 'shift'
+set args=
+:loop2
+    if "%~1" == "" goto end2
+    set args=%args% %~1
+    shift
+    goto loop2
+:end2
+
+set optimize=false
+if "%overwrite_optimize%" == "true" (
+    set optimize=true
+) else (
+    if "%default_optimize%" == "true" (
+        if not "%overwrite_optimize%" == "false" (
+            set optimize=true
+        )
+    )
+)
+
+if "%optimize%" == "true" (
+    set java_args=-XX:+AggressiveOpts -XX:+UseConcMarkSweepGC %java_args%
+) else (
+    set java_args=-XX:+AggressiveOpts -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none %java_args%
+)
+
+java %java_args% -jar %this% %jruby_args% %args% 
+
+endlocal
 
 exit /B
 BAT
--- embulk-cli/src/test/java/org/embulk/cli/DummyMain.java
+++ embulk-cli/src/test/java/org/embulk/cli/DummyMain.java
@@ -2,10 +2,12 @@ package org.embulk.cli;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.util.Arrays;
 
 public class DummyMain {
 
     public static void main(String[] args) throws Exception {
+        System.out.println(Arrays.asList(args));
         File thisFolder = new File(SelfrunTest.class.getResource("/org/embulk/cli/DummyMain.class").toURI()).getParentFile();
         try (FileWriter writer = new FileWriter(new File(thisFolder, "args.txt"))) {
             for (String arg : args) {
