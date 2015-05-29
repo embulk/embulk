3123d9525adfc0d6d1e8cc5acd7eca2f285f24c1
hito4t <thitoshi@cac.co.jp>
2015-05-29 19:16:29 +0900

    Fix findbugs warnings

--- embulk-cli/src/test/java/org/embulk/cli/DummyMain.java
+++ embulk-cli/src/test/java/org/embulk/cli/DummyMain.java
@@ -1,7 +1,10 @@
 package org.embulk.cli;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
 public class DummyMain {
@@ -9,10 +12,10 @@ public class DummyMain {
     public static void main(String[] args) throws Exception {
         System.out.println(Arrays.asList(args));
         File thisFolder = new File(SelfrunTest.class.getResource("/org/embulk/cli/DummyMain.class").toURI()).getParentFile();
-        try (FileWriter writer = new FileWriter(new File(thisFolder, "args.txt"))) {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(thisFolder, "args.txt")), Charset.defaultCharset()))) {
             for (String arg : args) {
                 writer.write(arg);
-                writer.write(System.getProperty("line.separator"));
+                writer.newLine();
             }
         }
     }
