be13fd6e8b36abc9fc65de88d20edd75b163da62
hito4t <thitoshi@cac.co.jp>
2015-05-29 11:16:35 +0900

    Test cases for selfrun.sh

--- embulk-cli/src/test/java/org/embulk/cli/SelfrunTest.java
+++ embulk-cli/src/test/java/org/embulk/cli/SelfrunTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class SelfrunTest {
 
@@ -25,6 +26,7 @@ public class SelfrunTest {
 
         File thisFolder = new File(SelfrunTest.class.getResource("/org/embulk/cli/SelfrunTest.class").toURI()).getParentFile();
         testSelfrun = new File(thisFolder, System.getProperty("file.separator").equals("\\") ? "selfrun.bat" : "selfrun.sh");
+        testSelfrun.setExecutable(true);
 
         File classpath = thisFolder.getParentFile().getParentFile().getParentFile();
         line = line.replaceAll("java ", "java -classpath " + classpath.getAbsolutePath().replaceAll("\\\\", "\\\\\\\\") + " org.embulk.cli.DummyMain ");
@@ -35,10 +37,192 @@ public class SelfrunTest {
 
 
     @Test
-    public void test() throws Exception {
-        System.out.println(execute());
+    public void testNoArgument() throws Exception {
+        List<String> args = execute();
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+TieredCompilation",
+                "-XX:TieredStopAtLevel=1",
+                "-Xverify:none",
+                "-jar",
+                testSelfrun.getAbsolutePath()),
+                args);
     }
 
+    @Test
+    public void testArguments() throws Exception {
+        List<String> args = execute("a1", "a2");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+TieredCompilation",
+                "-XX:TieredStopAtLevel=1",
+                "-Xverify:none",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "a1",
+                "a2"),
+                args);
+    }
+
+    @Test
+    public void testRun() throws Exception {
+        List<String> args = execute("run", "a1");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+UseConcMarkSweepGC",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "run",
+                "a1"),
+                args);
+    }
+
+    @Test
+    public void testJpO() throws Exception {
+        List<String> args = execute("-J+O", "a1", "a2");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+UseConcMarkSweepGC",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "a1",
+                "a2"),
+                args);
+    }
+
+    @Test
+    public void testJmO() throws Exception {
+        List<String> args = execute("-J-O", "a1", "a2");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+TieredCompilation",
+                "-XX:TieredStopAtLevel=1",
+                "-Xverify:none",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "a1",
+                "a2"),
+                args);
+    }
+
+    @Test
+    public void testR1() throws Exception {
+        List<String> args = execute("-Rr1", "a1", "a2");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+TieredCompilation",
+                "-XX:TieredStopAtLevel=1",
+                "-Xverify:none",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "r1",
+                "a1",
+                "a2"),
+                args);
+    }
+
+    @Test
+    public void testR2() throws Exception {
+        List<String> args = execute("-Rr1", "-Rr2", "a1", "a2");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+TieredCompilation",
+                "-XX:TieredStopAtLevel=1",
+                "-Xverify:none",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "r1",
+                "r2",
+                "a1",
+                "a2"),
+                args);
+    }
+
+    @Test
+    public void testRRun() throws Exception {
+        List<String> args = execute("-Rr1", "run", "a1");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+UseConcMarkSweepGC",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "r1",
+                "run",
+                "a1"),
+                args);
+    }
+
+    @Test
+    public void testJ1() throws Exception {
+        List<String> args = execute("-Jj1", "a1", "a2");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+TieredCompilation",
+                "-XX:TieredStopAtLevel=1",
+                "-Xverify:none",
+                "j1",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "a1",
+                "a2"),
+                args);
+    }
+
+    @Test
+    public void testJ2() throws Exception {
+        List<String> args = execute("-Jj1", "-Jj2", "a1", "a2");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+TieredCompilation",
+                "-XX:TieredStopAtLevel=1",
+                "-Xverify:none",
+                "j1",
+                "j2",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "a1",
+                "a2"),
+                args);
+    }
+
+    @Test
+    public void testJR() throws Exception {
+        List<String> args = execute("-Jj1", "-Rr1", "a1", "a2");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+TieredCompilation",
+                "-XX:TieredStopAtLevel=1",
+                "-Xverify:none",
+                "j1",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "r1",
+                "a1",
+                "a2"),
+                args);
+    }
+
+    @Test
+    public void testJFile() throws Exception {
+        File javaArgsFile = new File(testSelfrun.getParentFile(), "java_args.txt");
+        FileSystem fs = FileSystems.getDefault();
+        Files.write(fs.getPath(javaArgsFile.getAbsolutePath()), "j1 j2 j3".getBytes(), StandardOpenOption.CREATE);
+
+        List<String> args = execute("-J", javaArgsFile.getAbsolutePath(), "a1", "a2");
+        assertEquals(Arrays.asList(
+                "-XX:+AggressiveOpts",
+                "-XX:+TieredCompilation",
+                "-XX:TieredStopAtLevel=1",
+                "-Xverify:none",
+                "j1",
+                "j2",
+                "j3",
+                "-jar",
+                testSelfrun.getAbsolutePath(),
+                "a1",
+                "a2"),
+                args);
+    }
 
     private List<String> execute(String... arguments) throws Exception {
         List<String> commands = new ArrayList<String>();
@@ -48,8 +232,9 @@ public class SelfrunTest {
         process.waitFor();
 
         FileSystem fs = FileSystems.getDefault();
-        File args = new File(testSelfrun.getParentFile(), "args.txt");
-        return Files.readAllLines(fs.getPath(args.getAbsolutePath()), Charset.defaultCharset());
+        File argsFile = new File(testSelfrun.getParentFile(), "args.txt");
+        List<String> args = Files.readAllLines(fs.getPath(argsFile.getAbsolutePath()), Charset.defaultCharset());
+        return args;
     }
 
     private static File findSelfrun() {
