3abe910676c56baa43697729d2a5720370e160e0
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-07-17 11:12:41 -0700

    add 'embulk' command name to message of embulk-example

--- lib/embulk/command/embulk_run.rb
+++ lib/embulk/command/embulk_run.rb
@@ -290,9 +290,9 @@ examples:
       puts ""
       puts "Run following subcommands to try embulk:"
       puts ""
-      puts "   1. guess #{File.join(path, 'example.yml')} -o config.yml"
-      puts "   2. preview config.yml"
-      puts "   3. run config.yml"
+      puts "   1. embulk guess #{File.join(path, 'example.yml')} -o config.yml"
+      puts "   2. embulk preview config.yml"
+      puts "   3. embulk run config.yml"
       puts ""
 
     when :new
