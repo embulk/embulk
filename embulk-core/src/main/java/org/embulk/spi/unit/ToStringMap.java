9b693deae9c0af0e24e19c038bd3f793010dc6a0
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-07-23 16:41:56 -0700

    Add spi.unit.ToStringMap for a Task field which accepts fuzzy string-to-string map

---
+++ embulk-core/src/main/java/org/embulk/spi/unit/ToStringMap.java
@@ -0,0 +1,35 @@
+package org.embulk.spi.unit;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Properties;
+import com.google.common.base.Function;
+import com.google.common.collect.Maps;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public class ToStringMap
+        extends HashMap<String, String>
+{
+    @JsonCreator
+    public ToStringMap(Map<String, ToString> map)
+    {
+        super(Maps.transformValues(map, new Function<ToString, String>() {
+            public String apply(ToString value)
+            {
+                if (value == null) {
+                    return "null";
+                } else {
+                    return value.toString();
+                }
+            }
+        }));
+    }
+
+    public Properties toProperties()
+    {
+        Properties props = new Properties();
+        props.putAll(this);
+        return props;
+    }
+}
