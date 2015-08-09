d124e2ff9bb8c744ce29ab2a2245c4b6d3c6259a
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-08-09 19:11:26 +0900

    Clean up temp files recursively

--- embulk-core/src/main/java/org/embulk/spi/TempFileSpace.java
+++ embulk-core/src/main/java/org/embulk/spi/TempFileSpace.java
@@ -3,6 +3,11 @@ package org.embulk.spi;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.FileVisitResult;
 import java.io.File;
 import java.io.IOException;
 import com.google.common.base.Preconditions;
@@ -41,16 +46,35 @@ public class TempFileSpace
         }
     }
 
-    public void cleanup()
-    {
+    public void cleanup() {
         File[] files = dir.listFiles();
         if (files != null) {
             for (File e : files) {
-                e.delete();
-                // TODO delete directory recursively
+                try {
+                    deleteFileRecursively(e);
+                } catch (IOException ex) {
+                    // ignore IOException
+                }
             }
         }
         dir.delete();
         dirCreated = false;
+    }
+
+    private void deleteFileRecursively(File file) throws IOException {
+        Path directory = file.toPath();
+        Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 }
