e105b5543d7985e7262ee891794c11712f102ee8
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-08-13 21:35:13 -0700

    ruby can't raise a ruby class extending java exception

--- lib/embulk/error.rb
+++ lib/embulk/error.rb
@@ -1,12 +1,6 @@
 
 module Embulk
-  if java?
-    config_exception = org.embulk.config.ConfigException
-  else
-    config_exception = StandardError
-  end
-
-  class ConfigError < config_exception
+  class ConfigError < StandardError
   end
 
   class PluginLoadError < StandardError
