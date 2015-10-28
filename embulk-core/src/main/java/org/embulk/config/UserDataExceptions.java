224ad42f2edcc88cfcd631e8e6540ad33a357811
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-10-27 22:43:42 -0700

    UserDataExceptions.isUserDataException should be static method

--- embulk-core/src/main/java/org/embulk/config/UserDataExceptions.java
+++ embulk-core/src/main/java/org/embulk/config/UserDataExceptions.java
@@ -4,7 +4,7 @@ public class UserDataExceptions
 {
     private UserDataExceptions() { }
 
-    public boolean isUserDataException(Throwable exception)
+    public static boolean isUserDataException(Throwable exception)
     {
         while (exception != null) {
             if (exception instanceof UserDataException) {
