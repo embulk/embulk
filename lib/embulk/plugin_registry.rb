c46219f21a13b25de4a966ac79ff30ff7f84198e
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-12-15 10:45:53 +0900

    jruby can't raise an exception class extending a java exception class using 'raise ExceptionClass, "message"' style

--- lib/embulk/plugin_registry.rb
+++ lib/embulk/plugin_registry.rb
@@ -27,9 +27,9 @@ module Embulk
         if value = @map[type]
           return value
         end
-        raise PluginLoadError, "Unknown #{@category} plugin '#{type}'. #{@search_prefix}#{type}.rb is installed but it does not correctly register plugin."
+        raise PluginLoadError.new "Unknown #{@category} plugin '#{type}'. #{@search_prefix}#{type}.rb is installed but it does not correctly register plugin."
       else
-        raise PluginLoadError, "Unknown #{@category} plugin '#{type}'. #{@search_prefix}#{type}.rb is not installed. Run 'embulk gem search -rd embulk-#{@category}' command to find plugins."
+        raise PluginLoadError.new "Unknown #{@category} plugin '#{type}'. #{@search_prefix}#{type}.rb is not installed. Run 'embulk gem search -rd embulk-#{@category}' command to find plugins."
       end
     end
 
