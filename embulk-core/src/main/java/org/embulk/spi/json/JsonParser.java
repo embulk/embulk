acae00c1a7dd58a37119f3ad655de910a14f463d
Muga Nishizawa <muga.nishizawa@gmail.com>
2016-07-14 13:48:38 -0700

    Refactor spi.json.JsonParser * Create spi.json.JsonUtil and Move the functionality in JsonParser to JsonUtil. * Add @Deprecated to JsonParser.

--- embulk-core/src/main/java/org/embulk/spi/json/JsonParser.java
+++ embulk-core/src/main/java/org/embulk/spi/json/JsonParser.java
@@ -1,18 +1,13 @@
 package org.embulk.spi.json;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
 import java.io.InputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import org.msgpack.value.Value;
-import org.msgpack.value.ValueFactory;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser.Feature;
-import com.fasterxml.jackson.core.JsonToken;
 
+@Deprecated
 public class JsonParser
 {
     public interface Stream
@@ -143,90 +138,7 @@ public class JsonParser
 
         public Value next() throws IOException
         {
-            try {
-                JsonToken token = parser.nextToken();
-                if (token == null) {
-                    return null;
-                }
-                return jsonTokenToValue(token);
-            }
-            catch (com.fasterxml.jackson.core.JsonParseException ex) {
-                throw new JsonParseException("Failed to parse JSON: "+sampleJsonString(), ex);
-            }
-            catch (IOException ex) {
-                throw ex;
-            }
-            catch (JsonParseException ex) {
-                throw ex;
-            }
-            catch (RuntimeException ex) {
-                throw new JsonParseException("Failed to parse JSON: "+sampleJsonString(), ex);
-            }
-        }
-
-        private Value jsonTokenToValue(JsonToken token)
-            throws IOException
-        {
-            switch(token) {
-            case VALUE_NULL:
-                return ValueFactory.newNil();
-            case VALUE_TRUE:
-                return ValueFactory.newBoolean(true);
-            case VALUE_FALSE:
-                return ValueFactory.newBoolean(false);
-            case VALUE_NUMBER_FLOAT:
-                return ValueFactory.newFloat(parser.getDoubleValue());
-            case VALUE_NUMBER_INT:
-                try {
-                    return ValueFactory.newInteger(parser.getLongValue());
-                }
-                catch (JsonParseException ex) {
-                    return ValueFactory.newInteger(parser.getBigIntegerValue());
-                }
-            case VALUE_STRING:
-                return ValueFactory.newString(parser.getText());
-            case START_ARRAY: {
-                List<Value> list = new ArrayList<>();
-                while (true) {
-                    token = parser.nextToken();
-                    if (token == JsonToken.END_ARRAY) {
-                        return ValueFactory.newArray(list);
-                    }
-                    else if (token == null) {
-                        throw new JsonParseException("Unexpected end of JSON at "+parser.getTokenLocation() + " while expecting an element of an array: " + sampleJsonString());
-                    }
-                    list.add(jsonTokenToValue(token));
-                }
-            }
-            case START_OBJECT:
-                Map<Value, Value> map = new HashMap<>();
-                while (true) {
-                    token = parser.nextToken();
-                    if (token == JsonToken.END_OBJECT) {
-                        return ValueFactory.newMap(map);
-                    }
-                    else if (token == null) {
-                        throw new JsonParseException("Unexpected end of JSON at "+parser.getTokenLocation() + " while expecting a key of object: " + sampleJsonString());
-                    }
-                    String key = parser.getCurrentName();
-                    if (key == null) {
-                        throw new JsonParseException("Unexpected token "+token+" at "+parser.getTokenLocation() + ": " + sampleJsonString());
-                    }
-                    token = parser.nextToken();
-                    if (token == null) {
-                        throw new JsonParseException("Unexpected end of JSON at "+parser.getTokenLocation() + " while expecting a value of object: " + sampleJsonString());
-                    }
-                    Value value = jsonTokenToValue(token);
-                    map.put(ValueFactory.newString(key), value);
-                }
-            case VALUE_EMBEDDED_OBJECT:
-            case FIELD_NAME:
-            case END_ARRAY:
-            case END_OBJECT:
-            case NOT_AVAILABLE:
-            default:
-                throw new JsonParseException("Unexpected token "+token+" at "+parser.getTokenLocation() + ": " + sampleJsonString());
-            }
+            return JsonUtil.readJson(parser);
         }
     }
 }
