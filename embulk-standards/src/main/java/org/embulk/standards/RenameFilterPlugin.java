f36bb61282c60148b32b1d2351834b5bc6880642
Dai MIKURUBE <dmikurube@treasure-data.com>
2016-10-13 14:08:39 +0900

    Add a new renaming rule "case".

--- embulk-standards/src/main/java/org/embulk/standards/RenameFilterPlugin.java
+++ embulk-standards/src/main/java/org/embulk/standards/RenameFilterPlugin.java
@@ -11,7 +11,10 @@ import org.embulk.spi.FilterPlugin;
 import org.embulk.spi.PageOutput;
 import org.embulk.spi.Schema;
 
+import com.google.common.collect.ImmutableSet;
+
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 
@@ -82,12 +85,43 @@ public class RenameFilterPlugin
         String getRule();
     }
 
+    private interface CaseRule
+            extends Rule {
+        @Config("to")
+        String getTo();
+    }
+
     private Schema applyRule(ConfigSource ruleConfig, Schema inputSchema) throws ConfigException
     {
         Rule rule = ruleConfig.loadConfig(Rule.class);
         switch (rule.getRule()) {
+        case "case":
+            return applyCaseRule(inputSchema, ruleConfig.loadConfig(CaseRule.class));
         default:
             throw new ConfigException("Renaming rule \"" +rule+ "\" is unknown");
         }
     }
+
+    private Schema applyCaseRule(Schema inputSchema, CaseRule rule) {
+        final String to = rule.getTo();
+
+        if (!toInCase.contains(to)) {
+            throw new ConfigException("\"" +to+ "\" is unknown as \"to\" in rule \"case\"");
+        }
+
+        Schema.Builder builder = Schema.builder();
+        for (Column column : inputSchema.getColumns()) {
+            switch (to) {
+            case "lower":
+                builder.add(column.getName().toLowerCase(Locale.ENGLISH), column.getType());
+                break;
+            case "upper":
+                builder.add(column.getName().toUpperCase(Locale.ENGLISH), column.getType());
+                break;
+            }
+        }
+        return builder.build();
+    }
+
+    final ImmutableSet<String> toInCase = ImmutableSet.<String>of("lower", "upper");
 }
--- embulk-standards/src/test/java/org/embulk/standards/TestRenameFilterPlugin.java
+++ embulk-standards/src/test/java/org/embulk/standards/TestRenameFilterPlugin.java
@@ -12,9 +12,13 @@ import org.embulk.spi.Exec;
 import org.embulk.spi.Schema;
 import org.embulk.spi.SchemaConfigException;
 import org.embulk.standards.RenameFilterPlugin.PluginTask;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.embulk.spi.type.Types.STRING;
 import static org.embulk.spi.type.Types.TIMESTAMP;
@@ -22,10 +26,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+
 public class TestRenameFilterPlugin
 {
     @Rule
     public EmbulkTestRuntime runtime = new EmbulkTestRuntime();
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     private final Schema SCHEMA = Schema.builder()
             .add("_c0", STRING)
@@ -136,5 +144,77 @@ public class TestRenameFilterPlugin
         } catch (Throwable t) {
             assertTrue(t instanceof ConfigException);
         }
+    }
+
+    @Test
+    public void checkCaseRuleToLower()
+    {
+        final String original[] = { "_C0", "_C1", "_c2" };
+        final String expected[] = { "_c0", "_c1", "_c2" };
+        ConfigSource config = Exec.newConfigSource().set("rules",
+                ImmutableList.of(ImmutableMap.of("rule", "case", "to", "lower")));
+        renameAndCheckSchema(config, original, expected);
+    }
+
+    @Test
+    public void checkCaseRuleToUpper()
+    {
+        final String original[] = { "_C0", "_C1", "_c2" };
+        final String expected[] = { "_C0", "_C1", "_C2" };
+        ConfigSource config = Exec.newConfigSource().set("rules",
+                ImmutableList.of(ImmutableMap.of("rule", "case", "to", "upper")));
+        renameAndCheckSchema(config, original, expected);
+    }
+
+    @Test
+    public void checkCaseRuleToUnknown()
+    {
+        final String original[] = { "_C0", "_C1", "_c2" };
+        ConfigSource config = Exec.newConfigSource().set("rules",
+                ImmutableList.of(ImmutableMap.of("rule", "case", "to", "some_unknown_case")));
+        exception.expect(ConfigException.class);
+        // TODO(dmikurube): Except "Caused by": exception.expectCause(instanceOf(JsonMappingException.class));
+        // Needs to import org.hamcrest.Matchers... in addition to org.junit...
+        renameAndCheckSchema(config, original, original);
+    }
+
+    @Test
+    public void checkCaseRuleEmpty()
+    {
+        final String original[] = { "_C0", "_C1", "_c2" };
+        ConfigSource config = Exec.newConfigSource().set("rules",
+                ImmutableList.of(ImmutableMap.of("rule", "case")));
+        exception.expect(ConfigException.class);
+        // TODO(dmikurube): Except "Caused by": exception.expectCause(instanceOf(JsonMappingException.class));
+        // Needs to import org.hamcrest.Matchers... in addition to org.junit...
+        renameAndCheckSchema(config, original, original);
+    }
+
+    private Schema makeSchema(final String columnNames[])
+    {
+        Schema.Builder builder = new Schema.Builder();
+        for (String columnName : columnNames) {
+            builder.add(columnName, STRING);
+        }
+        return builder.build();
+    }
+
+    private void renameAndCheckSchema(ConfigSource config,
+                                      final String original[],
+                                      final String expected[])
+    {
+        final Schema originalSchema = makeSchema(original);
+        filter.transaction(config, originalSchema, new FilterPlugin.Control() {
+            @Override
+            public void run(TaskSource task, Schema renamedSchema)
+            {
+                assertEquals(originalSchema.getColumnCount(), renamedSchema.getColumnCount());
+                assertEquals(expected.length, renamedSchema.getColumnCount());
+                for (int i = 0; i < renamedSchema.getColumnCount(); ++i) {
+                    assertEquals(originalSchema.getColumnType(i), renamedSchema.getColumnType(i));
+                    assertEquals(expected[i], renamedSchema.getColumnName(i));
+                }
+            }
+        });
     }
 }
