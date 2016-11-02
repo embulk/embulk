f559ded8654422be62fa18ecaca740a0f47b7793
Sadayuki Furuhashi <frsyuki@gmail.com>
2016-11-02 14:34:48 -0700

    Add embulk-test package
    This package is intended to be used from the test cases of Java-based
    input plugins.
    
    Support for other plugin types is desired but not implemented in this
    change.

---
+++ embulk-test/src/main/java/org/embulk/test/EmbulkTestingEmbed.java
@@ -0,0 +1,175 @@
+package org.embulk.test;
+
+import com.google.common.io.ByteStreams;
+import com.google.inject.Binder;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.embulk.EmbulkEmbed;
+import org.embulk.config.Config;
+import org.embulk.config.ConfigDiff;
+import org.embulk.config.ConfigLoader;
+import org.embulk.config.ConfigSource;
+import org.embulk.exec.ExecutionResult;
+import org.embulk.spi.TempFileException;
+import org.embulk.spi.TempFileSpace;
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.embulk.plugin.InjectedPluginSource.registerPluginTo;
+
+public class EmbulkTestingEmbed
+{
+    public static class Builder
+    {
+        private List<Module> modules = new ArrayList<>();
+
+        Builder()
+        { }
+
+        public <T> Builder registerPlugin(final Class<T> iface, final String name, final Class<?> impl)
+        {
+            modules.add(new Module() {
+                public void configure(Binder binder)
+                {
+                    registerPluginTo(binder, iface, name, impl);
+                }
+            });
+            return this;
+        }
+
+        public EmbulkTestingEmbed build()
+        {
+            return new EmbulkTestingEmbed(this);
+        }
+    }
+
+    public static Builder builder()
+    {
+        return new Builder();
+    }
+
+    private final EmbulkEmbed embed;
+    private final TempFileSpace tempFiles;
+
+    EmbulkTestingEmbed(Builder builder)
+    {
+        this.embed = new EmbulkEmbed.Bootstrap()
+            .addModules(builder.modules)
+            .initializeCloseable();
+        try {
+            this.tempFiles = new TempFileSpace(Files.createTempDirectory("embulk-test-temp-").toFile());
+        }
+        catch (IOException ex) {
+            throw new TempFileException(ex);
+        }
+    }
+
+    public void destroy()
+    {
+        embed.destroy();
+        tempFiles.cleanup();
+    }
+
+    public Path createTempFile(String suffix)
+    {
+        return tempFiles.createTempFile(suffix).toPath();
+    }
+
+    public Injector injector()
+    {
+        return embed.getInjector();
+    }
+
+    public ConfigLoader configLoader()
+    {
+        return embed.newConfigLoader();
+    }
+
+    public ConfigSource newConfig()
+    {
+        return configLoader().newConfigSource();
+    }
+
+    public ConfigSource loadYamlResource(String name)
+    {
+        return configLoader()
+            .fromYamlString(EmbulkTests.readResource(name));
+    }
+
+    public static class RunResult
+    {
+        private final ConfigDiff configDiff;
+        private final List<Throwable> ignoredExceptions;
+
+        RunResult(ConfigDiff configDiff, List<Throwable> ignoredExceptions)
+        {
+            this.configDiff = configDiff;
+            this.ignoredExceptions = ignoredExceptions;
+        }
+
+        public ConfigDiff getConfigDiff()
+        {
+            return configDiff;
+        }
+
+        public List<Throwable> getIgnoredExceptions()
+        {
+            return ignoredExceptions;
+        }
+    }
+
+    public RunResult runInput(ConfigSource inConfig, Path outputPath)
+        throws IOException
+    {
+        String fileName = outputPath.getFileName().toString();
+        checkArgument(fileName.endsWith(".csv"), "outputPath must end with .csv");
+        Path dir = outputPath.getParent().resolve(fileName.substring(0, fileName.length() - 4));
+
+        Files.createDirectories(dir);
+
+        ConfigSource execConfig = newConfig()
+            .set("min_output_tasks", 1);
+
+        ConfigSource outConfig = newConfig()
+            .set("type", "file")
+            .set("path_prefix", dir.resolve("fragments_").toString())
+            .set("file_ext", "csv")
+            .set("formatter", newConfig()
+                    .set("type", "csv")
+                    .set("header_line", false)
+                    .set("newline", "LF"));
+
+        ConfigSource config = newConfig()
+            .set("exec", execConfig)
+            .set("in", inConfig)
+            .set("out", outConfig);
+
+        ExecutionResult result = embed.run(config);
+
+        try (OutputStream out = Files.newOutputStream(outputPath)) {
+            List<Path> fragments = new ArrayList<Path>();
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "fragments_*.csv")) {
+                for (Path fragment : stream) {
+                    fragments.add(fragment);
+                }
+            }
+            Collections.sort(fragments);
+            for (Path fragment : fragments) {
+                try (InputStream in = Files.newInputStream(fragment)) {
+                    ByteStreams.copy(in, out);
+                }
+            }
+        }
+
+        return new RunResult(
+            result.getConfigDiff(),
+            result.getIgnoredExceptions());
+    }
+}
