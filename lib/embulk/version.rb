cb187e8b08a6089775927b24fff069c899ef692e
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-03-27 18:16:13 +0900

    Warn usage of Embulk::VERSION just once.

--- lib/embulk/version.rb
+++ lib/embulk/version.rb
@@ -1,15 +1,20 @@
 # TODO(v2)[#562]: Remove this file.
 # https://github.com/embulk/embulk/issues/562
 module Embulk
+  @@warned = false
+
   VERSION_INTERNAL = '0.8.18'
 
   DEPRECATED_MESSAGE = 'Embulk::VERSION in (J)Ruby is deprecated. Use org.embulk.EmbulkVersion::VERSION instead. If this message is from a plugin, please tell this to the author of the plugin!'
   def self.const_missing(name)
     if name == :VERSION
-      if Embulk.method_defined?(:logger)
-        Embulk.logger.warn(DEPRECATED_MESSAGE)
-      else
-        STDERR.puts(DEPRECATED_MESSAGE)
+      unless @@warned
+        if Embulk.method_defined?(:logger)
+          Embulk.logger.warn(DEPRECATED_MESSAGE)
+        else
+          STDERR.puts(DEPRECATED_MESSAGE)
+        end
+        @@warned = true
       end
       return VERSION_INTERNAL
     else
