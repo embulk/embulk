9840e9d3015ec16a17154e7545ff2937e620c506
Civitaspo <civitaspo@gmail.com>
2017-05-24 01:03:22 +0900

    Remove unnecessary error validations

--- lib/embulk/error.rb
+++ lib/embulk/error.rb
@@ -3,25 +3,11 @@ module Embulk
   # ConfigError is not a ::StandardError but is a java.lang.RuntimeException.
   # "rescue => e" can rescues ConfigError.
   class ConfigError < Java::Config::ConfigException
-    def initialize(message=nil)
-      if message
-        super(message.to_s)
-      else
-        super()
-      end
-    end
   end
 
   # DataError is not a ::StandardError but is a java.lang.RuntimeException.
   # "rescue => e" can rescues DataError.
   class DataError < Java::SPI::DataException
-    def initialize(message=nil)
-      if message
-        super(message.to_s)
-      else
-        super()
-      end
-    end
   end
 
   class PluginLoadError < ConfigError
