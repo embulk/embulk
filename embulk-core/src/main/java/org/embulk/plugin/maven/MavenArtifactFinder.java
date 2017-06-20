9b606d6bc1feaec54ce7da158ac01e10f93b3988
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-06-20 13:22:14 +0900

    Add MavenArtifactFinder to find a plugin Maven artifact from a local Maven repository.

---
+++ embulk-core/src/main/java/org/embulk/plugin/maven/MavenArtifactFinder.java
@@ -0,0 +1,122 @@
+package org.embulk.plugin.maven;
+
+import java.io.IOError;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.NotDirectoryException;
+import java.nio.file.Path;
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.repository.LocalRepository;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
+
+public class MavenArtifactFinder
+{
+    private MavenArtifactFinder(final Path givenLocalMavenRepositoryPath,
+                                final Path absoluteLocalMavenRepositoryPath,
+                                final RepositorySystem repositorySystem,
+                                final RepositorySystemSession repositorySystemSession)
+    {
+        this.givenLocalMavenRepositoryPath = givenLocalMavenRepositoryPath;
+        this.absoluteLocalMavenRepositoryPath = absoluteLocalMavenRepositoryPath;
+        this.repositorySystem = repositorySystem;
+        this.repositorySystemSession = repositorySystemSession;
+    }
+
+    public static MavenArtifactFinder create(final Path localMavenRepositoryPath)
+        throws MavenRepositoryNotFoundException
+    {
+        final Path absolutePath;
+        try {
+            absolutePath = localMavenRepositoryPath.normalize().toAbsolutePath();
+        }
+        catch (IOError ex) {
+            throw new MavenRepositoryNotFoundException(localMavenRepositoryPath, ex);
+        }
+        catch (SecurityException ex) {
+            throw new MavenRepositoryNotFoundException(localMavenRepositoryPath, ex);
+        }
+
+        if (!Files.exists(absolutePath)) {
+            throw new MavenRepositoryNotFoundException(localMavenRepositoryPath,
+                                                       absolutePath,
+                                                       new NoSuchFileException(absolutePath.toString()));
+        }
+        if (!Files.isDirectory(absolutePath)) {
+            throw new MavenRepositoryNotFoundException(localMavenRepositoryPath,
+                                                       absolutePath,
+                                                       new NotDirectoryException(absolutePath.toString()));
+        }
+
+        final RepositorySystem repositorySystem = createRepositorySystem();
+
+        return new MavenArtifactFinder(localMavenRepositoryPath,
+                                       absolutePath,
+                                       repositorySystem,
+                                       createRepositorySystemSession(repositorySystem, absolutePath));
+    }
+
+    public final Path findMavenArtifactJar(
+            final String groupId, final String artifactId, final String version)
+        throws MavenArtifactNotFoundException
+    {
+        return findMavenArtifact(groupId, artifactId, "jar", version);
+    }
+
+    public Path findMavenArtifact(
+            final String groupId, final String artifactId, final String extension, final String version)
+        throws MavenArtifactNotFoundException
+    {
+        final ArtifactResult result;
+        try {
+            result = resolveMavenArtifact(groupId, artifactId, extension, version);
+        }
+        catch (ArtifactResolutionException ex) {
+            throw new MavenArtifactNotFoundException(groupId, artifactId, version,
+                                                     this.givenLocalMavenRepositoryPath,
+                                                     this.absoluteLocalMavenRepositoryPath,
+                                                     ex);
+        }
+        return result.getArtifact().getFile().toPath();
+    }
+
+    private ArtifactResult resolveMavenArtifact(
+            final String groupId, final String artifactId, final String extension, final String version)
+        throws ArtifactResolutionException
+    {
+        final ArtifactRequest artifactRequest =
+            new ArtifactRequest().setArtifact(new DefaultArtifact(groupId, artifactId, extension, version));
+
+        return this.repositorySystem.resolveArtifact(this.repositorySystemSession, artifactRequest);
+    }
+
+    private static RepositorySystem createRepositorySystem()
+    {
+        final DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
+        return locator.getService(RepositorySystem.class);
+    }
+
+    private static RepositorySystemSession createRepositorySystemSession(
+            final RepositorySystem repositorySystem, final Path localRepositoryPath)
+    {
+        final DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
+
+        final LocalRepository repository = new LocalRepository(localRepositoryPath.toString());
+        session.setLocalRepositoryManager(repositorySystem.newLocalRepositoryManager(session, repository));
+        return session;
+    }
+
+    // Paths are kept just for hinting in Exceptions.
+    private final Path givenLocalMavenRepositoryPath;
+    private final Path absoluteLocalMavenRepositoryPath;
+
+    private final RepositorySystem repositorySystem;
+    private final RepositorySystemSession repositorySystemSession;
+}
---
+++ embulk-core/src/test/java/org/embulk/plugin/maven/TestMavenArtifactFinder.java
@@ -0,0 +1,41 @@
+package org.embulk.plugin.maven;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URL;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.Test;
+
+public class TestMavenArtifactFinder
+{
+    @Test
+    public void testArtifacts() throws Exception
+    {
+        final Path basePath = getMavenPath();
+        final MavenArtifactFinder finder = MavenArtifactFinder.create(basePath);
+        assertEquals(buildExpectedPath(basePath, GROUP_DIRECTORY, "embulk-example-maven-artifact", "0.1.2"),
+                     finder.findMavenArtifactJar("org.embulk.example", "embulk-example-maven-artifact", "0.1.2"));
+    }
+
+    private Path getMavenPath() throws URISyntaxException
+    {
+        return Paths.get(this.getClass().getClassLoader().getResource("m2.test").toURI());
+    }
+
+    private Path buildExpectedPath(
+            final Path basePath, final Path groupDirectory, final String artifactId, final String version)
+    {
+        return basePath
+            .resolve(groupDirectory)
+            .resolve(artifactId)
+            .resolve(version)
+            .resolve(artifactId + "-" + version + ".jar");
+    }
+
+    private static final Path GROUP_DIRECTORY = Paths.get("org").resolve("embulk").resolve("example");
+}
