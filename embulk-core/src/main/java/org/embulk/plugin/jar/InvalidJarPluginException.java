e9d6ae239c94139c1cd0d73c470e1f2b237f214e
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-06-21 01:14:50 +0900

    Add JarPluginLoader to load a JAR-based plugin class.

---
+++ embulk-core/src/main/java/org/embulk/plugin/jar/InvalidJarPluginException.java
@@ -0,0 +1,14 @@
+package org.embulk.plugin.jar;
+
+public class InvalidJarPluginException
+        extends Exception {
+    public InvalidJarPluginException(final String message)
+    {
+        super(message);
+    }
+
+    public InvalidJarPluginException(final String message, final Throwable cause)
+    {
+        super(message, cause);
+    }
+}
