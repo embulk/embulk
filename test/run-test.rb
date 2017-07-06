6bb4073b522095cbd2d12651ae8173d52eddd216
Muga Nishizawa <muga.nishizawa@gmail.com>
2017-07-06 15:28:16 -0700

    Fix test/run-test.rb to use #611's RubyDateParser and StrptimeParser in ruby tests

--- test/run-test.rb
+++ test/run-test.rb
@@ -5,7 +5,21 @@ test_dir = File.join(base_dir, "test")
 $LOAD_PATH.unshift(lib_dir)
 $LOAD_PATH.unshift(test_dir)
 
+require "date"
 require "test/unit"
+
+module DateExt
+  def self.included base
+    base.instance_eval do
+      def _strptime(str, fmt='%F')
+        parser = org.embulk.spi.time.RubyDateParser.new
+        map = parser.parse(JRuby.runtime.current_context, fmt, str)
+        return map.nil? ? nil : map.to_hash.inject({}){|hash,(k,v)| hash[k.to_sym] = v; hash}
+      end
+    end
+  end
+end
+Date.send(:include, DateExt)
 
 Dir.glob("#{base_dir}/test/**/test{_,-}*.rb") do |file|
   require file.sub(/\.rb$/,"")
