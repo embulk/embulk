4f4da64939f8721b2beb922cffe302a1107729fa
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-10-13 14:47:56 +0900

    Remove dead code around the Ruby timestamp parser replaced

--- embulk-core/src/main/java/org/embulk/spi/time/JRubyTimeParserHelperFactory.java
+++
@@ -1,6 +0,0 @@
-package org.embulk.spi.time;
-
-public interface JRubyTimeParserHelperFactory
-{
-    JRubyTimeParserHelper newInstance(String formatString, int year, int mon, int day, int hour, int min, int sec, int usec);
-}
