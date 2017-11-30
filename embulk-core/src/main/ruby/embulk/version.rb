b24d8802ece8cfb97ff6360a950472ed7b1a01ee
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-11-30 17:29:14 +0900

    Make Ruby Embulk::VERSION based on Java org.embulk.EmbulkVersion.VERSION
    It embeds Embulk versions in manifests of subprojects (embulk-core and others) in addition.

--- embulk-core/src/main/ruby/embulk/version.rb
+++ embulk-core/src/main/ruby/embulk/version.rb
@@ -1,24 +1,5 @@
-# TODO(v2)[#562]: Remove this file.
-# https://github.com/embulk/embulk/issues/562
 module Embulk
-  @@warned = false
-
-  VERSION_INTERNAL = '0.9.0.snapshot'
-
-  DEPRECATED_MESSAGE = 'Embulk::VERSION in (J)Ruby is deprecated. Use org.embulk.EmbulkVersion::VERSION instead. If this message is from a plugin, please tell this to the author of the plugin!'
-  def self.const_missing(name)
-    if name == :VERSION
-      unless @@warned
-        if Embulk.method_defined?(:logger)
-          Embulk.logger.warn(DEPRECATED_MESSAGE)
-          @@warned = true
-        else
-          STDERR.puts(DEPRECATED_MESSAGE)
-        end
-      end
-      return VERSION_INTERNAL
-    else
-      super
-    end
-  end
+  # Converts the original Java-style version string to Ruby-style.
+  # E.g., "0.9.0-SNAPSHOT" (in Java) is converted to "0.9.0.snapshot" in Ruby.
+  VERSION = ::String.new(Java::org.embulk.EmbulkVersion::VERSION).tr('-', '.').downcase
 end
