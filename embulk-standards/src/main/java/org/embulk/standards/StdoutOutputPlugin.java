446b0a6a1127926a1b9f8780b5a812d28878ffe9
Dai MIKURUBE <dmikurube@treasure-data.com>
2018-02-10 23:25:25 +0900

    Revert "Fix StdoutOutputPlugin broken by stopping @Config indirection by default implementation at: #956"
    This reverts commit 603c46bdd9c190e28b9f29251689cd97a0bdcfbc.

--- embulk-standards/src/main/java/org/embulk/standards/StdoutOutputPlugin.java
+++ embulk-standards/src/main/java/org/embulk/standards/StdoutOutputPlugin.java
@@ -26,9 +26,13 @@ public class StdoutOutputPlugin implements OutputPlugin {
         // Using Joda-Time is deprecated, but the getter returns org.joda.time.DateTimeZone for plugin compatibility.
         // It won't be removed very soon at least until Embulk v0.10.
         @Deprecated
-        @Config("timezone")
-        @ConfigDefault("\"UTC\"")
-        public org.joda.time.DateTimeZone getTimeZone();
+        public default org.joda.time.DateTimeZone getTimeZone() {
+            if (getTimeZoneId() != null) {
+                return TimeZoneIds.parseJodaDateTimeZone(getTimeZoneId());
+            } else {
+                return null;
+            }
+        }
     }
 
     @Override
