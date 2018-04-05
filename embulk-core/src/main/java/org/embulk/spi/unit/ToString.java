71ce8cfc9ddcf0e1af216493ea7d70b6d3b36caa
Dai MIKURUBE <dmikurube@treasure-data.com>
2018-04-05 17:41:45 +0900

    Use java.util.Optional in org.embulk.spi.unit.ToString

--- embulk-core/src/main/java/org/embulk/spi/unit/ToString.java
+++ embulk-core/src/main/java/org/embulk/spi/unit/ToString.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.NullNode;
-import com.google.common.base.Optional;
+import java.util.Optional;
 
 public class ToString {
     private final String string;
@@ -14,9 +14,23 @@ public class ToString {
         this.string = string;
     }
 
-    @JsonCreator
-    public ToString(Optional<JsonNode> option) throws JsonMappingException {
+    // This constructor with com.google.common.base.Optional is kept for compatibility for direct callers.
+    @Deprecated
+    public ToString(com.google.common.base.Optional<JsonNode> option) throws JsonMappingException {
         JsonNode node = option.or(NullNode.getInstance());
+        if (node.isTextual()) {
+            this.string = node.textValue();
+        } else if (node.isValueNode()) {
+            this.string = node.toString();
+        } else {
+            throw new JsonMappingException(String.format("Arrays and objects are invalid: '%s'", node));
+        }
+    }
+
+    // This @JsonCreator constructor with java.util.Optional is called through Jackson's databind.
+    @JsonCreator
+    public ToString(final Optional<JsonNode> option) throws JsonMappingException {
+        final JsonNode node = option.orElse(NullNode.getInstance());
         if (node.isTextual()) {
             this.string = node.textValue();
         } else if (node.isValueNode()) {
