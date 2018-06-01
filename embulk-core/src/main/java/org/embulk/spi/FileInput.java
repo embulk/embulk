dea0426b8343add3d2508b1cb272685d19bb286c
Dai MIKURUBE <dmikurube@treasure-data.com>
2018-06-01 10:49:32 +0900

    [WIP] Add name and expectedSize in FileInput

--- embulk-core/src/main/java/org/embulk/spi/FileInput.java
+++ embulk-core/src/main/java/org/embulk/spi/FileInput.java
@@ -1,9 +1,19 @@
 package org.embulk.spi;
 
+import java.util.Optional;
+
 public interface FileInput extends AutoCloseable {
     boolean nextFile();
 
     Buffer poll();
 
     void close();
+
+    default Optional<String> name() {
+        return Optional.empty();
+    }
+
+    default Optional<Long> expectedSize() {
+        return Optional.empty();
+    }
 }
