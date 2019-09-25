a1f56e9ca4791e8362bbf3e345a2650be0a2c9fc
Dai MIKURUBE <dmikurube@treasure-data.com>
2019-09-25 17:01:59 +0900

    Update the repo for https://www.embulk.org/docs from "www.embulk.org" to "docs"
    We had had maintained the entire www.embulk.org website in the repo "www.embulk.org".
    But, the repo is now renamed to "docs". This commit is to catch up with the latest status.
    
    https://www.embulk.org/docs/ has been the main website, and it has been updated from
    the "embulk-docs" directory of this "embulk" repository.
    
    Later on, we plan to manage the website like:
    * https://www.embulk.org/* (except for docs): https://github.com/embulk/embulk.github.io
    * https://www.embulk.org/docs/: in https://github.com/embulk/docs

--- embulk-docs/push-gh-pages.sh
+++ embulk-docs/push-gh-pages.sh
@@ -16,7 +16,7 @@ function r() {
 [ "$TRAVIS_BRANCH" != "master" -a "$TRAVIS_BRANCH" != "$(git describe --tags --always HEAD)" ] && exit 0
 
 revision="$(git rev-parse HEAD)"
-remote="https://github.com/embulk/www.embulk.org.git"
+remote="https://github.com/embulk/docs.git"
 re ./gradlew site
 
 r git fetch --unshallow || echo "using complete repository."
