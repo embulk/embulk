18a9575a72bdef7c4cb1cf609eaad6a503b08dfb
Dai MIKURUBE <dmikurube@treasure-data.com>
2019-10-29 18:04:38 +0900

    Guarantee TempFileSpace to be unique
    A TempFileSpace has been created in a directory with a timestamp, such as "/tmp/2019-05-15 11:35:48.617 UTC".
    But, when multiple Embulk processes (or tasks) are running, they can easily conflict.
    
    To avoid such conflicts, this change starts to use Files#createTempDirectory to create a TempFileSpace.
    Java VM guarantees that the directory created is unique.
    
    https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html#createTempDirectory-java.nio.file.Path-java.lang.String-java.nio.file.attribute.FileAttribute...-
    
    The old constructor TempFileSpace(File) keeps the original behavior for compatibility,
    but it is now deprecated. It should not be called from plugins, though.

--- embulk-core/src/main/java/org/embulk/exec/SimpleTempFileSpaceAllocator.java
+++ embulk-core/src/main/java/org/embulk/exec/SimpleTempFileSpaceAllocator.java
@@ -1,29 +1,48 @@
 package org.embulk.exec;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.embulk.spi.TempFileSpace;
 import org.embulk.spi.TempFileSpaceAllocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class SimpleTempFileSpaceAllocator implements TempFileSpaceAllocator {
     SimpleTempFileSpaceAllocator() {
         // It is originally intended to use `temp_dirs` in system config, but the reasons are missing.
         // https://github.com/embulk/embulk/commit/a7643573ecb39e6dd71a08edce77c8e64dc70a77
-        final String systemPropertyTmpDir = System.getProperty("java.io.tmpdir", DEFAULT_TEMP_DIR);
-        this.dir = new File(systemPropertyTmpDir.isEmpty() ? DEFAULT_TEMP_DIR : systemPropertyTmpDir, "embulk");
+
+        final String systemPropertyTmpDir = System.getProperty("java.io.tmpdir");
+        if (systemPropertyTmpDir == null || systemPropertyTmpDir.isEmpty()) {
+            // TODO: Error when java.io.tmpdir is not set?
+            logger.warn("Property java.io.tmpdir should be set properly.");
+            this.tempDirectoryBase = Paths.get(DEFAULT_TEMP_DIR);
+        } else {
+            this.tempDirectoryBase = Paths.get(systemPropertyTmpDir);
+        }
     }
 
     @Override
-    public TempFileSpace newSpace(final String subdirectoryName) {
+    public TempFileSpace newSpace(final String subdirectoryPrefix) {
         // It is originally intended to support multiple files/directories, but the reasons are missing.
         // https://github.com/embulk/embulk/commit/a7643573ecb39e6dd71a08edce77c8e64dc70a77
         // https://github.com/embulk/embulk/commit/5a78270a4fc20e3c113c68e4c0f6c66c1bd45886
 
         // UNIX/Linux cannot include '/' as file name.
         // Windows cannot include ':' as file name.
-        return new TempFileSpace(new File(this.dir, subdirectoryName.replace('/', '-').replace(':', '-')));
+        try {
+            return TempFileSpace.with(
+                    this.tempDirectoryBase, "embulk" + subdirectoryPrefix.replace('/', '-').replace(':', '-'));
+        } catch (final IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
     }
 
     private static final String DEFAULT_TEMP_DIR = "/tmp";
 
-    private final File dir;
+    private static final Logger logger = LoggerFactory.getLogger(SimpleTempFileSpaceAllocator.class);
+
+    private final Path tempDirectoryBase;
 }
