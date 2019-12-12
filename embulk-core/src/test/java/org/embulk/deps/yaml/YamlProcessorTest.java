41a85f36c120487058d5764cbd3e49917df6031b
shroman <rshtykh@yahoo.com>
2019-12-12 12:14:37 +0900

    Load SnakeYaml in a separate DependencyClassLoader. checkstyle fix.

--- embulk-core/src/test/java/org/embulk/deps/yaml/YamlProcessorTest.java
+++ embulk-core/src/test/java/org/embulk/deps/yaml/YamlProcessorTest.java
@@ -1,14 +1,12 @@
 package org.embulk.deps.yaml;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-public class YamlProcessorTest
-{
+public class YamlProcessorTest {
     @Test
-    public void create()
-    {
+    public void create() {
         assertTrue(YamlProcessor.create(false) instanceof YamlProcessorImpl);
         assertTrue(YamlProcessor.create(true) instanceof YamlProcessorImpl);
     }
