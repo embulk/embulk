351628666e3d204fa6895f0e38051d99420c0644
Dai MIKURUBE <dmikurube@treasure-data.com>
2020-06-24 16:39:34 +0900

    Merge duplicated implementations of Instant-to-String and String-to-Instant

--- embulk-core/src/main/java/org/embulk/spi/time/Timestamp.java
+++ embulk-core/src/main/java/org/embulk/spi/time/Timestamp.java
@@ -1,12 +1,14 @@
 package org.embulk.spi.time;
 
 import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 
 public class Timestamp implements Comparable<Timestamp> {
     private Timestamp(final Instant instant) {
         this.instant = instant;
+    }
+
+    public static Timestamp ofString(final String string) {
+        return new Timestamp(Instants.parseInstant(string));
     }
 
     public static Timestamp ofInstant(final Instant instant) {
@@ -65,39 +67,8 @@ public class Timestamp implements Comparable<Timestamp> {
 
     @Override
     public String toString() {
-        final int nano = this.instant.getNano();
-        if (nano == 0) {
-            return FORMATTER_SECONDS.format(this.instant) + " UTC";
-        } else if (nano % 1000000 == 0) {
-            return FORMATTER_MILLISECONDS.format(this.instant) + " UTC";
-        } else {
-            final StringBuilder builder = new StringBuilder();
-            FORMATTER_SECONDS.formatTo(this.instant, builder);
-            builder.append(".");
-
-            final String digits;
-            final int zeroDigits;
-            if (nano % 1000 == 0) {
-                digits = Integer.toString(nano / 1000);
-                zeroDigits = 6 - digits.length();
-            } else {
-                digits = Integer.toString(nano);
-                zeroDigits = 9 - digits.length();
-            }
-            builder.append(digits);
-            for (int i = 0; i < zeroDigits; i++) {
-                builder.append('0');
-            }
-
-            builder.append(" UTC");
-            return builder.toString();
-        }
+        return Instants.toString(this.instant);
     }
-
-    private static final DateTimeFormatter FORMATTER_SECONDS =
-            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneOffset.UTC);
-    private static final DateTimeFormatter FORMATTER_MILLISECONDS =
-            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").withZone(ZoneOffset.UTC);
 
     private final Instant instant;
 }
--- embulk-core/src/main/java/org/embulk/spi/time/TimestampSerDe.java
+++ embulk-core/src/main/java/org/embulk/spi/time/TimestampSerDe.java
@@ -9,21 +9,16 @@ import com.fasterxml.jackson.databind.deser.std.FromStringDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.module.guice.ObjectMapperModule;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class TimestampSerDe {
     public static void configure(ObjectMapperModule mapper) {
         SimpleModule module = new SimpleModule();
-        module.addSerializer(Timestamp.class, new DateTimeZoneSerializer());
-        module.addDeserializer(Timestamp.class, new DateTimeZoneDeserializer());
+        module.addSerializer(Timestamp.class, new TimestampSerializer());
+        module.addDeserializer(Timestamp.class, new TimestampDeserializer());
         mapper.registerModule(module);
     }
 
-    public static class DateTimeZoneSerializer extends JsonSerializer<Timestamp> {
+    public static class TimestampSerializer extends JsonSerializer<Timestamp> {
         @Override
         public void serialize(Timestamp value, JsonGenerator jgen, SerializerProvider provider)
                 throws IOException {
@@ -31,44 +26,24 @@ public class TimestampSerDe {
         }
     }
 
-    public static class DateTimeZoneDeserializer extends FromStringDeserializer<Timestamp> {
-        public DateTimeZoneDeserializer() {
+    public static class TimestampDeserializer extends FromStringDeserializer<Timestamp> {
+        public TimestampDeserializer() {
             super(Timestamp.class);
         }
 
         @Override
         protected Timestamp _deserialize(String value, DeserializationContext context)
                 throws JsonMappingException {
-            return createTimestampFromString(value);
+            if (value == null) {
+                throw new JsonMappingException("TimestampDeserializer#_deserialize received null unexpectedly.");
+            }
+            try {
+                return Timestamp.ofString(value);
+            } catch (final NumberFormatException ex) {
+                throw new JsonMappingException("Invalid format as a Timestamp value: '" + value + "'", ex);
+            } catch (final IllegalStateException ex) {
+                throw new JsonMappingException("Unexpected failure in parsing: '" + value + "'", ex);
+            }
         }
     }
-
-    static Timestamp createTimestampFromStringForTesting(final String string) {
-        return createTimestampFromString(string);
-    }
-
-    private static Timestamp createTimestampFromString(final String string) {
-        // TODO: Handle exceptions.
-        final Matcher matcher = TIMESTAMP_PATTERN.matcher(string);
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException(String.format("Invalid timestamp format '%s'", string));
-        }
-
-        final long epochSecond = LocalDateTime.parse(matcher.group(1), FORMATTER).toEpochSecond(ZoneOffset.UTC);
-
-        final String fraction = matcher.group(2);
-        final int nanoAdjustment;
-        if (fraction == null) {
-            nanoAdjustment = 0;
-        } else {
-            nanoAdjustment = Integer.parseInt(fraction) * (int) Math.pow(10, 9 - fraction.length());
-        }
-
-        return Timestamp.ofEpochSecond(epochSecond, nanoAdjustment);
-    }
-
-    private static final DateTimeFormatter FORMATTER =
-            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneOffset.UTC);
-    private static final Pattern TIMESTAMP_PATTERN =
-            Pattern.compile("(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})(?:\\.(\\d{1,9}))? (?:UTC|\\+?00\\:?00)");
 }
