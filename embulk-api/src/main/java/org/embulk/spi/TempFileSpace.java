e205d8955fbbe0c6859fd6c76054bb6174515b15
Dai MIKURUBE <dmikurube@treasure-data.com>
2020-09-14 15:02:26 +0900

    Add Javadocs and a license boilerplate on TempFileSpace

--- embulk-api/src/main/java/org/embulk/spi/TempFileSpace.java
+++ embulk-api/src/main/java/org/embulk/spi/TempFileSpace.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 The Embulk project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.embulk.spi;
 
 import java.io.File;
@@ -5,20 +21,29 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 /**
- * Responsible to manage a temporary directory (a space for temporary files) for a task.
+ * Manages a space for temporary files.
  *
- * <p>Plugins are expected to get this through {@link Exec#getTempFileSpace}. Do not create an instance of this directly.
+ * <p>Plugins are expected to get this through {@link org.embulk.spi.Exec#getTempFileSpace}.
  */
 public abstract class TempFileSpace {
+    /**
+     * Exists only for {@code TempFileSpaceImpl}'s constructor as {@link TempFileSpace} has another deprecated constructor.
+     */
     TempFileSpace() {
     }
 
+    /**
+     * @deprecated The constructor is no longer available. Use {@link org.embulk.spi.Exec#getTempFileSpace} instead.
+     */
     @Deprecated
     public TempFileSpace(final File dir) {
         throw new UnsupportedOperationException(
                 "TempFileSpace can no longer be constructed directly. Use Exec.getTempFileSpace() instead.");
     }
 
+    /**
+     * @deprecated The static creator method is no longer available. Use {@link org.embulk.spi.Exec#getTempFileSpace} instead.
+     */
     @Deprecated
     public static TempFileSpace with(final Path baseDir, final String prefix) throws IOException {
         throw new UnsupportedOperationException(
@@ -26,26 +51,26 @@ public abstract class TempFileSpace {
     }
 
     /**
-     * Creates a temporary file with the default file name prefix, and the default file extension suffix.
+     * Creates a temporary file in the space, with the default file name prefix, and the default file extension suffix.
      *
-     * @return A temporary {@link java.io.File} created
+     * @return a temporary {@link java.io.File} created
      */
     public abstract File createTempFile();
 
     /**
-     * Creates a temporary file with the default file name prefix, and the specified file extension suffix.
+     * Creates a temporary file in the space, with the default file name prefix, and the specified file extension suffix.
      *
-     * @param fileExt  The file extension suffix without a leading dot ({@code '.'})
-     * @return A temporary {@link java.io.File} created
+     * @param fileExt  the file extension suffix without a leading dot ({@code '.'})
+     * @return a temporary {@link java.io.File} created
      */
     public abstract File createTempFile(final String fileExt);
 
     /**
-     * Creates a temporary file with the specified file name prefix, and the specified file extension suffix.
+     * Creates a temporary file in the space, with the specified file name prefix, and the specified file extension suffix.
      *
-     * @param prefix  The file name prefix
-     * @param fileExt  The file extension suffix without a leading dot ({@code '.'})
-     * @return A temporary {@link java.io.File} created
+     * @param prefix  the file name prefix
+     * @param fileExt  the file extension suffix without a leading dot ({@code '.'})
+     * @return a temporary {@link java.io.File} created
      */
     public abstract File createTempFile(final String prefix, final String fileExt);
 
