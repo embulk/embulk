b562d9d8e7a36ef8ef5524788e25da9b11367ee7
Dai MIKURUBE <dmikurube@treasure-data.com>
2020-11-27 16:46:21 +0900

    Move PreviewPrinter to embulk-deps

--- embulk-deps/src/main/java/org/embulk/deps/preview/TablePreviewPrinter.java
+++ embulk-deps/src/main/java/org/embulk/deps/preview/TablePreviewPrinter.java
@@ -1,41 +1,72 @@
-package org.embulk.command;
+package org.embulk.deps.preview;
 
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.embulk.spi.Page;
 import org.embulk.spi.Schema;
 
-public class TablePreviewPrinter extends PreviewPrinter {
-    private static final int MAX_SAMPLE_SIZE = 32000;
+final class TablePreviewPrinter extends PreviewPrinter {
+    TablePreviewPrinter(final PrintStream out, final Schema schema) {
+        this.out = out;
+        this.schema = schema;
+        this.valueFormatter = new ValueFormatter();
 
-    private List<String[]> samples;
-    private int sampleSize;
-    private String format;
-    private String border;
-
-    public TablePreviewPrinter(final PrintStream out, final Schema schema) {
-        super(out, schema);
-        this.samples = new ArrayList<String[]>();
         this.format = null;
         this.border = null;
-        String[] header = new String[schema.getColumnCount()];
+
+        this.sampleSize = 0;
+
+        this.samples = new ArrayList<String[]>();
+        final String[] header = new String[schema.getColumnCount()];
         for (int i = 0; i < header.length; i++) {
             header[i] = schema.getColumnName(i) + ":" + schema.getColumnType(i);
         }
-        samples.add(header);
+        this.samples.add(header);
     }
 
     @Override
-    protected void printRecord(String[] values) throws IOException {
+    @SuppressWarnings("deprecation")  // https://github.com/embulk/embulk/issues/1306
+    public void printAllPages(final List<Page> pages) throws IOException {
+        final List<Object[]> records = org.embulk.spi.util.Pages.toObjects(schema, pages, true);
+        for (final Object[] record : records) {
+            printRecord(record);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        out.close();
+    }
+
+    @Override
+    public void finish() throws IOException {
+        if (samples != null) {
+            flushSamples();
+        }
+        out.println(border);
+    }
+
+    private void printRecord(final Object... values) throws IOException {
+        final String[] stringValues = new String[schema.getColumnCount()];
+
+        int min = Math.min(schema.getColumnCount(), values.length);
+        for (int i = 0; i < min; i++) {
+            stringValues[i] = this.valueFormatter.valueToString(values[i]);
+        }
+        for (int i = min; i < schema.getColumnCount(); i++) {
+            stringValues[i] = this.valueFormatter.valueToString(null);
+        }
+
         if (samples == null) {
             // header is already written
-            out.format(format, Arrays.copyOf(values, values.length, Object[].class));
+            out.format(format, Arrays.copyOf(stringValues, stringValues.length, Object[].class));
         } else {
             // estimating size of columns
-            samples.add(Arrays.copyOf(values, values.length));
-            for (String v : values) {
+            samples.add(Arrays.copyOf(stringValues, stringValues.length));
+            for (String v : stringValues) {
                 sampleSize += v.length();
             }
             if (sampleSize > MAX_SAMPLE_SIZE) {
@@ -91,11 +122,15 @@ public class TablePreviewPrinter extends PreviewPrinter {
         return max;
     }
 
-    @Override
-    public void finish() throws IOException {
-        if (samples != null) {
-            flushSamples();
-        }
-        out.println(border);
-    }
+    private static final int MAX_SAMPLE_SIZE = 32000;
+
+    private final PrintStream out;
+    private final Schema schema;
+
+    private final ValueFormatter valueFormatter;
+
+    private List<String[]> samples;
+    private int sampleSize;
+    private String format;
+    private String border;
 }
