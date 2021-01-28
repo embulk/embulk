fec371ed851cd3ca4bb5c0da7cfd7d2d1d8a9460
Dai MIKURUBE <dmikurube@treasure-data.com>
2021-01-28 17:27:41 +0900

    Reimplement JSON guess in Java as embulk-guess-json

--- embulk-standards/src/test/java/org/embulk/standards/guess/TestCsvAllStringsGuessPlugin.java
+++ embulk-standards/src/test/java/org/embulk/standards/guess/TestCsvAllStringsGuessPlugin.java
@@ -11,6 +11,7 @@ import org.embulk.config.ConfigSource;
 import org.embulk.config.DataSource;
 import org.embulk.guess.bzip2.Bzip2GuessPlugin;
 import org.embulk.guess.gzip.GzipGuessPlugin;
+import org.embulk.guess.json.JsonGuessPlugin;
 import org.embulk.spi.FileInputPlugin;
 import org.embulk.spi.FileOutputPlugin;
 import org.embulk.spi.FormatterPlugin;
@@ -44,6 +45,7 @@ public class TestCsvAllStringsGuessPlugin {
             .registerPlugin(ParserPlugin.class, "csv", CsvParserPlugin.class)
             .registerPlugin(GuessPlugin.class, "bzip2", Bzip2GuessPlugin.class)
             .registerPlugin(GuessPlugin.class, "gzip", GzipGuessPlugin.class)
+            .registerPlugin(GuessPlugin.class, "json", JsonGuessPlugin.class)
             .build();
 
     @Test
--- embulk-standards/src/test/java/org/embulk/standards/guess/TestCsvGuessPlugin.java
+++ embulk-standards/src/test/java/org/embulk/standards/guess/TestCsvGuessPlugin.java
@@ -12,6 +12,7 @@ import org.embulk.config.ConfigSource;
 import org.embulk.config.DataSource;
 import org.embulk.guess.bzip2.Bzip2GuessPlugin;
 import org.embulk.guess.gzip.GzipGuessPlugin;
+import org.embulk.guess.json.JsonGuessPlugin;
 import org.embulk.spi.FileInputPlugin;
 import org.embulk.spi.FileOutputPlugin;
 import org.embulk.spi.FormatterPlugin;
@@ -45,6 +46,7 @@ public class TestCsvGuessPlugin {
             .registerPlugin(ParserPlugin.class, "csv", CsvParserPlugin.class)
             .registerPlugin(GuessPlugin.class, "bzip2", Bzip2GuessPlugin.class)
             .registerPlugin(GuessPlugin.class, "gzip", GzipGuessPlugin.class)
+            .registerPlugin(GuessPlugin.class, "json", JsonGuessPlugin.class)
             .build();
 
     @Test
