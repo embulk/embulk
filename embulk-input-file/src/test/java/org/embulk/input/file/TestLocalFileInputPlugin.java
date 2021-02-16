728e5c086b161b808be8585507ea2820ce38aac9
Dai MIKURUBE <dmikurube@treasure-data.com>
2021-02-16 16:19:37 +0900

    Move LocalFileInputPlugin to package org.embulk.input.file

--- embulk-input-file/src/test/java/org/embulk/input/file/TestLocalFileInputPlugin.java
+++ embulk-input-file/src/test/java/org/embulk/input/file/TestLocalFileInputPlugin.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.embulk.standards;
+package org.embulk.input.file;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
