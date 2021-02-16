4ffc8db06b26421e4c2a60f7788efce069afe737
Dai MIKURUBE <dmikurube@treasure-data.com>
2021-02-16 14:03:21 +0900

    Modernize embulk-decoder-bzip2 with v0.11-ready API/SPI

--- embulk-decoder-bzip2/src/main/java/org/embulk/standards/Bzip2FileDecoderPlugin.java
+++ embulk-decoder-bzip2/src/main/java/org/embulk/standards/Bzip2FileDecoderPlugin.java
@@ -1,45 +1,57 @@
+/*
+ * Copyright 2016 The Embulk project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.embulk.standards;
 
 import java.io.IOException;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
-import org.embulk.config.ConfigInject;
 import org.embulk.config.ConfigSource;
-import org.embulk.config.Task;
 import org.embulk.config.TaskSource;
-import org.embulk.spi.BufferAllocator;
 import org.embulk.spi.DecoderPlugin;
+import org.embulk.spi.Exec;
 import org.embulk.spi.FileInput;
-import org.embulk.spi.util.FileInputInputStream;
-import org.embulk.spi.util.InputStreamFileInput;
-import org.embulk.spi.util.InputStreamFileInput.InputStreamWithHints;
+import org.embulk.util.config.ConfigMapperFactory;
+import org.embulk.util.config.Task;
+import org.embulk.util.file.FileInputInputStream;
+import org.embulk.util.file.InputStreamFileInput;
 
 public class Bzip2FileDecoderPlugin implements DecoderPlugin {
-    public interface PluginTask extends Task {
-        @ConfigInject
-        BufferAllocator getBufferAllocator();
-    }
+    public interface PluginTask extends Task {}
 
     @Override
+    @SuppressWarnings("deprecation")  // For the use of task#dump().
     public void transaction(ConfigSource config, DecoderPlugin.Control control) {
-        PluginTask task = config.loadConfig(PluginTask.class);
+        final PluginTask task = CONFIG_MAPPER_FACTORY.createConfigMapper().map(config, PluginTask.class);
         control.run(task.dump());
     }
 
     @Override
     public FileInput open(TaskSource taskSource, FileInput fileInput) {
-        PluginTask task = taskSource.loadTask(PluginTask.class);
         final FileInputInputStream files = new FileInputInputStream(fileInput);
         return new InputStreamFileInput(
-                task.getBufferAllocator(),
+                Exec.getBufferAllocator(),
                 new InputStreamFileInput.Provider() {
                     // Implement openNextWithHints() instead of openNext() to show file name at parser plugin loaded by FileInputPlugin
                     // Because when using decoder, parser plugin can't get file name.
                     @Override
-                    public InputStreamWithHints openNextWithHints() throws IOException {
+                    public InputStreamFileInput.InputStreamWithHints openNextWithHints() throws IOException {
                         if (!files.nextFile()) {
                             return null;
                         }
-                        return new InputStreamWithHints(
+                        return new InputStreamFileInput.InputStreamWithHints(
                                 new BZip2CompressorInputStream(files, true),
                                 fileInput.hintOfCurrentInputFileNameForLogging().orElse(null)
                         );
@@ -51,4 +63,6 @@ public class Bzip2FileDecoderPlugin implements DecoderPlugin {
                     }
                 });
     }
+
+    private static final ConfigMapperFactory CONFIG_MAPPER_FACTORY = ConfigMapperFactory.builder().addDefaultModules().build();
 }
