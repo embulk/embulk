524c5c3e048d124f5a20368b0ddf76aa01524d3a
Hiroyuki Sato <hiroysato@gmail.com>
2022-06-14 21:56:50 +0900

    Use single super statement (Fix #1513)

--- embulk-ruby/lib/embulk/error.rb
+++ embulk-ruby/lib/embulk/error.rb
@@ -1,26 +1,24 @@
-
+#
+# As of JRuby 9.3, subclassing a Java classes are full java classes,
+# which introduces some restrictions:
+# See details:
+# https://github.com/jruby/jruby/wiki/CallingJavaFromJRuby#subclassing-a-java-class
+# https://github.com/jruby/jruby/issues/7221
+#
 module Embulk
   # ConfigError is not a ::StandardError but is a java.lang.RuntimeException.
   # "rescue => e" can rescues ConfigError.
   class ConfigError < Java::Config::ConfigException
-    def initialize(message=nil)
-      if message
-        super(message.to_s)
-      else
-        super()
-      end
+    def initialize(*arguments)
+      super
     end
   end
 
   # DataError is not a ::StandardError but is a java.lang.RuntimeException.
   # "rescue => e" can rescues DataError.
   class DataError < Java::SPI::DataException
-    def initialize(message=nil)
-      if message
-        super(message.to_s)
-      else
-        super()
-      end
+    def initialize(*arguments)
+      super
     end
   end
 
