1022e79332921975b69b4afdb17fe0bda859be7c
Hiroyuki Sato <hiroysato@gmail.com>
2022-06-30 20:09:43 +0900

    Use rdoc format

--- embulk-ruby/lib/embulk/error.rb
+++ embulk-ruby/lib/embulk/error.rb
@@ -1,13 +1,14 @@
-#
-# As of JRuby 9.3, subclassing a Java classes are full java classes,
-# which introduces some restrictions:
-# See details:
-# https://github.com/jruby/jruby/wiki/CallingJavaFromJRuby#subclassing-a-java-class
-# https://github.com/jruby/jruby/issues/7221
-#
+
 module Embulk
   # ConfigError is not a ::StandardError but is a java.lang.RuntimeException.
   # "rescue => e" can rescues ConfigError.
+  #
+  # As of JRuby 9.3, subclassing a Java classes are full java classes,
+  # which introduces some restrictions.
+  #
+  # See details:
+  # - https://github.com/jruby/jruby/wiki/CallingJavaFromJRuby#subclassing-a-java-class
+  # - https://github.com/jruby/jruby/issues/7221
   class ConfigError < Java::Config::ConfigException
     def initialize(*arguments)
       super
@@ -16,6 +17,13 @@ module Embulk
 
   # DataError is not a ::StandardError but is a java.lang.RuntimeException.
   # "rescue => e" can rescues DataError.
+  #
+  # As of JRuby 9.3, subclassing a Java classes are full java classes,
+  # which introduces some restrictions.
+  #
+  # See details:
+  # - https://github.com/jruby/jruby/wiki/CallingJavaFromJRuby#subclassing-a-java-class
+  # - https://github.com/jruby/jruby/issues/7221
   class DataError < Java::SPI::DataException
     def initialize(*arguments)
       super
